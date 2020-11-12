204e137676201ccdc6cfe76789d811c2d496f0de
rhugonne <romain.hugonnet@gmail.com>
2020-11-12 10:32:12 +0100

    add folder structure + nmad

---
+++ DemUtils/spatial_tools.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def nmad(data, nfact=1.4826):
+    """
+    Calculate the normalized median absolute deviation (NMAD) of an array.
+
+    :param data: input data
+    :param nfact: normalization factor for the data; default is 1.4826
+
+    :type data: array-like
+    :type nfact: float
+    :returns nmad: (normalized) median absolute deviation of data.
+    """
+    m = np.nanmedian(data)
+    return nfact * np.nanmedian(np.abs(data - m))
