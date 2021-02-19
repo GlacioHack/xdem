390a2e0105ca13fd16f48a77334b678416e7388f
Erik Holmlund <erik-holmlund@hotmail.com>
2021-02-19 11:06:43 +0100

    Updated coregistration tests
    Former-commit-id: 90e0b5f0b4bec8ba4f47f7621894ae8051b9f106

--- tests/test_coreg.py
+++ tests/test_coreg.py
@@ -8,6 +8,7 @@ import geoutils as gu
 import pytest
 
 from DemUtils import coreg
+
 EXAMPLE_PATHS = {
     "dem1": "examples/Longyearbyen/data/DEM_2009_ref.tif",
     "dem2": "examples/Longyearbyen/data/DEM_1995.tif",
@@ -15,33 +16,62 @@ EXAMPLE_PATHS = {
 }
 
 
+def load_examples() -> tuple[gu.georaster.Raster, gu.georaster.Raster, gu.geovector.Vector]:
+    reference_raster = gu.georaster.Raster(EXAMPLE_PATHS["dem1"])
+    to_be_aligned_raster = gu.georaster.Raster(EXAMPLE_PATHS["dem2"])
+    glacier_mask = gu.geovector.Vector(EXAMPLE_PATHS["glacier_mask"])
+    return reference_raster, to_be_aligned_raster, glacier_mask
+
+
+def test_coreg_method_enum():
+    """Test that the CoregMethod enum works as it should."""
+    # Try to generate an enum from a string
+    icp = coreg.CoregMethod.from_str("icp")
+    # Make sure the enum points to the right function
+    assert icp == coreg.icp_coregistration
+
+    # Make sure the following madness ends up in a ValueError
+    try:
+        coreg.CoregMethod.from_str("klawld")
+
+        raise AssertionError
+    except ValueError:
+        pass
+
+
 class TestCoreg:
+    """Test different types of coregistration methods."""
 
-    def test_icp(self):
-        """Test the ICP coregistration method."""
-        reference_raster = gu.georaster.Raster(EXAMPLE_PATHS["dem1"])
-        to_be_aligned_raster = gu.georaster.Raster(EXAMPLE_PATHS["dem2"])
-        glacier_mask = gu.geovector.Vector(EXAMPLE_PATHS["glacier_mask"])
-
-        _, error = coreg.coregister(
-            reference_raster, to_be_aligned_raster, method="icp", mask=glacier_mask)
-
-        assert error < 10
+    ref, tba, mask = load_examples()  # Load example reference, to-be-aligned and mask.
 
     def test_amaury(self):
         """Test the Amaury/ Nuth & Kääb method."""
-        reference_raster = gu.georaster.Raster(EXAMPLE_PATHS["dem1"])
-        to_be_aligned_raster = gu.georaster.Raster(EXAMPLE_PATHS["dem2"])
-        glacier_mask = gu.geovector.Vector(EXAMPLE_PATHS["glacier_mask"])
-
-        _, error = coreg.coregister(
-            reference_raster, to_be_aligned_raster, method="amaury", mask=glacier_mask)
+        _, error = coreg.coregister(self.ref, self.tba, method="amaury", mask=self.mask)
 
         assert error < 10
 
-    def test_only_paths(self):
-        """Test that raster paths can be specified instead of Raster objects."""
-        reference_raster = EXAMPLE_PATHS["dem1"]
-        to_be_aligned_raster = EXAMPLE_PATHS["dem2"]
+    def test_amaury_high_degree(self):
+        """Test the Amaury / Nuth & Kääb method with nonlinear deramping."""
+        _, error = coreg.coregister(self.ref, self.tba, mask=self.mask, method="icp", deramping_degree=3)
 
-        coreg.coregister(reference_raster, to_be_aligned_raster)
+        assert error < 10
+
+    def test_deramping(self):
+        """Test the deramping coregistration method."""
+        _, error = coreg.coregister(self.ref, self.tba, method="deramp", mask=self.mask)
+
+        assert error < 10
+
+    def test_icp(self):
+        """Test the ICP coregistration method."""
+        _, error = coreg.coregister(self.ref, self.tba, method="icp", mask=self.mask)
+
+        assert error < 10
+
+
+def test_only_paths():
+    """Test that raster paths can be specified instead of Raster objects."""
+    reference_raster = EXAMPLE_PATHS["dem1"]
+    to_be_aligned_raster = EXAMPLE_PATHS["dem2"]
+
+    coreg.coregister(reference_raster, to_be_aligned_raster)
