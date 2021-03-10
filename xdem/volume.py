786b3853973682703581a75158177370c0e2e65d
Erik Holmlund <erik-holmlund@hotmail.com>
2021-03-10 13:39:28 +0100

    Added bin size (in georeferenced units or percent) as an argument and added documentation

--- xdem/volume.py
+++ xdem/volume.py
@@ -1,31 +1,52 @@
 from __future__ import annotations
 
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
 
-def hypsometric_binning(ddem: np.ndarray, dem: np.ndarray):
+def hypsometric_binning(ddem: np.ndarray, dem: np.ndarray, bin_size=50,
+                        normalized_bin_size: bool = False) -> pd.DataFrame:
+    """
+    Separate the dDEM in discrete elevation bins.
+
+    :param ddem: The dDEM as a 2D or 1D array.
+    :param dem: The reference DEM as a 2D or 1D array.
+    :param bin_size: The bin interval size in georeferenced units (or percent; 0-100, if normalized_bin_size=True)
+    :param normalized_bin_size: If the given bin size should be parsed as a percentage of the glacier's elevation range.
+
+    :returns: A Pandas DataFrame with elevation bins and dDEM statistics.
+    """
 
     assert ddem.shape == dem.shape
+    # Remove all nans, and flatten the inputs.
     nan_mask = np.isnan(ddem) | np.isnan(dem)
     ddem = ddem[~nan_mask]
     dem = dem[~nan_mask]
 
-    old_dem = dem - ddem
-    elevation_range = np.percentile(old_dem, 99) - np.percentile(old_dem, 1)
+    # Calculate the mean representative elevations between the two DEMs
+    mean_dem = dem - (ddem / 2)
 
-    bin_size = 50 if elevation_range > 500 else elevation_range / 10
+    # If the bin size should be seen as a percentage.
+    if normalized_bin_size:
+        assert bin_size > 0 and bin_size < 100
 
-    bins = np.arange(old_dem.min(), old_dem.max() + bin_size, step=bin_size)
-    indices = np.digitize(old_dem, bins=bins)
+        # Get the statistical elevation range to normalize the bin size with
+        elevation_range = np.percentile(mean_dem, 99) - np.percentile(mean_dem, 1)
+        bin_size = elevation_range / bin_size
 
+    # Generate bins and get bin indices from the mean DEM
+    bins = np.arange(mean_dem.min(), mean_dem.max() + bin_size, step=bin_size)
+    indices = np.digitize(mean_dem, bins=bins)
+
+    # Calculate statistics for each bin.
+    # If no values exist, all stats should be nans (except count with should be 0)
     medians = np.zeros(shape=bins.shape[0] - 1, dtype=ddem.dtype) + np.nan
     means = medians.copy()
     stds = medians.copy()
     counts = np.zeros_like(medians, dtype=int)
-    for i in np.unique(indices):
+    for i in np.arange(indices.min(), indices.max() + 1):
         values_in_bin = ddem[indices == i]
+        # Skip if no values are in the bin.
         if values_in_bin.shape[0] == 0:
             continue
 
@@ -34,6 +55,7 @@ def hypsometric_binning(ddem: np.ndarray, dem: np.ndarray):
         stds[i - 1] = np.std(values_in_bin)
         counts[i - 1] = values_in_bin.shape[0]
 
+    # Collect the results in a dataframe
     output = pd.DataFrame(
         index=pd.IntervalIndex.from_breaks(bins),
         data=np.vstack([
@@ -41,9 +63,5 @@ def hypsometric_binning(ddem: np.ndarray, dem: np.ndarray):
         ]).T,
         columns=["median", "mean", "std", "count"]
     )
-
-    # more options can be specified also
-    print(output.to_string())
-    raise ValueError
 
     return output
