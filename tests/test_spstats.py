b65f0432a54bb0834845654a080a14e062b5f886
rhugonne <romain.hugonnet@gmail.com>
2021-03-11 15:04:20 +0100

    add test for variogram integration

--- tests/test_spstats.py
+++ tests/test_spstats.py
@@ -39,22 +39,54 @@ class TestEmpiricalVariogram:
         df = xdem.spstats.get_empirical_variogram(dh=diff.data.flatten()[0:1000],coords=coords[0:1000,:])
 
         # check the wrapper script runs with various inputs
-        df_gsd = xdem.spstats.sample_multirange_empirical_variogram(dh=diff.data,gsd=diff.res[0],bin_func='even')
+        # with gsd as input
+        df_gsd = xdem.spstats.sample_multirange_empirical_variogram(dh=diff.data,gsd=diff.res[0])
 
+        # with coords as input, and "uniform" bin_func
         df_coords = xdem.spstats.sample_multirange_empirical_variogram(dh=diff.data.flatten(),coords=coords,
                                                                        bin_func='uniform')
 
-        df_1000_bins = xdem.spstats.sample_multirange_empirical_variogram(dh=diff.data,gsd=diff.res[0],
-                                                                          bin_func='even',n_lags=1000)
+        # using more bins
+        df_1000_bins = xdem.spstats.sample_multirange_empirical_variogram(dh=diff.data,gsd=diff.res[0],n_lags=1000)
 
-        df_sig = xdem.spstats.sample_multirange_empirical_variogram(dh=diff.data,gsd=diff.res[0],bin_func='even',
-                                                                    nsamp=1000,nrun=30,nproc=10)
+        # using multiple runs with parallelized function
+        df_sig = xdem.spstats.sample_multirange_empirical_variogram(dh=diff.data,gsd=diff.res[0], nsamp=1000,
+                                                                    nrun=20, nproc=10, maxlag = 10000)
 
-        fun = xdem.spstats.fit_model_sum_vgm(['Sph'],df_sig)
+        # test plotting
+        xdem.spstats.plot_vgm(df_sig)
 
-        fun2 = xdem.spstats.fit_model_sum_vgm(['Sph','Sph','Sph'],emp_vgm_df=df_sig)
+        # single model fit
+        fun, _ = xdem.spstats.fit_model_sum_vgm(['Sph'],df_sig)
+        xdem.spstats.plot_vgm(df_sig,fit_fun=fun)
 
+        # triple model fit
+        fun2, _ = xdem.spstats.fit_model_sum_vgm(['Sph','Sph','Sph'],emp_vgm_df=df_sig)
+        xdem.spstats.plot_vgm(df_sig,fit_fun=fun2)
 
+    def test_neff_estimation(self):
+
+        # test the precision of numerical integration for several spherical models
+
+        # short range
+        crange1 = [10**i for i in range(8)]
+        # long range
+        crange2 = [100*sr for sr in crange1]
+
+        p1 = 0.8
+        p2 = 0.2
+
+        for r1 in crange1:
+            r2 = crange2[crange1.index(r1)]
+
+            # and for any glacier area
+            for area in [10**i for i in range(10)]:
+
+                neff_circ_exact = xdem.spstats.exact_neff_sphsum_circular(area=area,crange1=r1,psill1=p1,crange2=r2
+                                                                          ,psill2=p2)
+                neff_circ_numer = xdem.spstats.neff_circ(area,[(r1,'Sph',p1),(r2,'Sph',p2)])
+
+                assert np.abs(neff_circ_exact-neff_circ_exact)<0.001
 
 
 
