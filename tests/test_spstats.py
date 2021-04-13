1f734567944d7fcd354a9a5dd31200a00224352d
Erik Holmlund <erik-holmlund@hotmail.com>
2021-04-13 15:16:43 +0200

    Improved tests

--- tests/test_spstats.py
+++ tests/test_spstats.py
@@ -3,14 +3,19 @@ Functions to test the spatial statistics.
 
 """
 from __future__ import annotations
+
+import geoutils as gu
 import numpy as np
+import pandas as pd
+from skgstat import models
+
 import xdem
 from xdem import examples
-import geoutils as gu
-from skgstat import models
-import pandas as pd
 
-def load_diff() -> tuple[gu.georaster.Raster, np.ndarray] :
+PLOT = False
+
+
+def load_diff() -> tuple[gu.georaster.Raster, np.ndarray]:
     """Load example files to try coregistration methods with."""
     examples.download_longyearbyen_examples(overwrite=False)
 
@@ -20,12 +25,13 @@ def load_diff() -> tuple[gu.georaster.Raster, np.ndarray] :
 
     metadata = {}
     aligned_raster, _ = xdem.coreg.coregister(reference_raster, to_be_aligned_raster, method="amaury", mask=glacier_mask,
-                                     metadata=metadata)
+                                              metadata=metadata)
 
-    diff = xdem.spatial_tools.subtract_rasters(reference_raster,aligned_raster)
+    diff = xdem.spatial_tools.subtract_rasters(reference_raster, aligned_raster)
     mask = glacier_mask.create_mask(diff)
 
     return diff, mask
+
 
 class TestVariogram:
 
@@ -37,59 +43,61 @@ class TestVariogram:
         x, y = diff.coords(offset='center')
         coords = np.dstack((x.flatten(), y.flatten())).squeeze()
 
-
         # check the base script runs with right input shape
-        df = xdem.spstats.get_empirical_variogram(dh=diff.data.flatten()[0:1000],coords=coords[0:1000,:])
+        df = xdem.spstats.get_empirical_variogram(dh=diff.data.flatten()[0:1000], coords=coords[0:1000, :])
 
         # check the wrapper script runs with various inputs
         # with gsd as input
-        df_gsd = xdem.spstats.sample_multirange_empirical_variogram(dh=diff.data,gsd=diff.res[0])
+        df_gsd = xdem.spstats.sample_multirange_empirical_variogram(dh=diff.data, gsd=diff.res[0])
 
         # with coords as input, and "uniform" bin_func
-        df_coords = xdem.spstats.sample_multirange_empirical_variogram(dh=diff.data.flatten(),coords=coords,
+        df_coords = xdem.spstats.sample_multirange_empirical_variogram(dh=diff.data.flatten(), coords=coords,
                                                                        bin_func='uniform')
 
         # using more bins
-        df_1000_bins = xdem.spstats.sample_multirange_empirical_variogram(dh=diff.data,gsd=diff.res[0],n_lags=1000)
+        df_1000_bins = xdem.spstats.sample_multirange_empirical_variogram(dh=diff.data, gsd=diff.res[0], n_lags=1000)
 
         # using multiple runs with parallelized function
-        df_sig = xdem.spstats.sample_multirange_empirical_variogram(dh=diff.data,gsd=diff.res[0], nsamp=1000,
-                                                                    nrun=20, nproc=10, maxlag = 10000)
+        df_sig = xdem.spstats.sample_multirange_empirical_variogram(dh=diff.data, gsd=diff.res[0], nsamp=1000,
+                                                                    nrun=20, nproc=10, maxlag=10000)
 
         # test plotting
-        xdem.spstats.plot_vgm(df_sig)
+        if PLOT:
+            xdem.spstats.plot_vgm(df_sig)
 
         # single model fit
-        fun, _ = xdem.spstats.fit_model_sum_vgm(['Sph'],df_sig)
-        xdem.spstats.plot_vgm(df_sig,fit_fun=fun)
+        fun, _ = xdem.spstats.fit_model_sum_vgm(['Sph'], df_sig)
+        if PLOT:
+            xdem.spstats.plot_vgm(df_sig, fit_fun=fun)
 
         # triple model fit
-        fun2, _ = xdem.spstats.fit_model_sum_vgm(['Sph','Sph','Sph'],emp_vgm_df=df_sig)
-        xdem.spstats.plot_vgm(df_sig,fit_fun=fun2)
+        fun2, _ = xdem.spstats.fit_model_sum_vgm(['Sph', 'Sph', 'Sph'], emp_vgm_df=df_sig)
+        if PLOT:
+            xdem.spstats.plot_vgm(df_sig, fit_fun=fun2)
 
     def test_multirange_fit_performance(self):
 
         # first, generate a true sum of variograms with some added noise
-        r1, ps1, r2, ps2, r3, ps3 = (100,0.7,1000,0.2,10000,0.1)
+        r1, ps1, r2, ps2, r3, ps3 = (100, 0.7, 1000, 0.2, 10000, 0.1)
 
-        x = np.linspace(10,20000,500)
-        y = models.spherical(x,r=r1,c0=ps1) + models.spherical(x,r=r2,c0=ps2) \
-            + models.spherical(x,r=r3,c0=ps3)
+        x = np.linspace(10, 20000, 500)
+        y = models.spherical(x, r=r1, c0=ps1) + models.spherical(x, r=r2, c0=ps2) \
+            + models.spherical(x, r=r3, c0=ps3)
 
         sig = 0.025
-        y_noise = np.random.normal(0,sig,size=len(x))
+        y_noise = np.random.normal(0, sig, size=len(x))
 
         y_simu = y + y_noise
         sigma = np.ones(len(x))*sig
 
         df = pd.DataFrame()
-        df = df.assign(bins=x,exp=y_simu,exp_sigma=sig)
+        df = df.assign(bins=x, exp=y_simu, exp_sigma=sig)
 
         # then, run the fitting
-        fun, params = xdem.spstats.fit_model_sum_vgm(['Sph','Sph','Sph'],df)
+        fun, params = xdem.spstats.fit_model_sum_vgm(['Sph', 'Sph', 'Sph'], df)
 
-        xdem.spstats.plot_vgm(df,fit_fun=fun)
-
+        if PLOT:
+            xdem.spstats.plot_vgm(df, fit_fun=fun)
 
     def test_neff_estimation(self):
 
@@ -109,11 +117,11 @@ class TestVariogram:
             # and for any glacier area
             for area in [10**i for i in range(10)]:
 
-                neff_circ_exact = xdem.spstats.exact_neff_sphsum_circular(area=area,crange1=r1,psill1=p1,crange2=r2
-                                                                          ,psill2=p2)
-                neff_circ_numer = xdem.spstats.neff_circ(area,[(r1,'Sph',p1),(r2,'Sph',p2)])
+                neff_circ_exact = xdem.spstats.exact_neff_sphsum_circular(
+                    area=area, crange1=r1, psill1=p1, crange2=r2, psill2=p2)
+                neff_circ_numer = xdem.spstats.neff_circ(area, [(r1, 'Sph', p1), (r2, 'Sph', p2)])
 
-                assert np.abs(neff_circ_exact-neff_circ_numer)<0.001
+                assert np.abs(neff_circ_exact-neff_circ_numer) < 0.001
 
 
 class TestPatchesMethod:
@@ -123,7 +131,4 @@ class TestPatchesMethod:
         diff, mask = load_diff()
 
         # check the patches method runs
-        df_patches = xdem.spstats.patches_method(diff.data,mask=~mask.astype(bool),gsd=diff.res[0],area_size=10000)
-
-
-
+        df_patches = xdem.spstats.patches_method(diff.data, mask=~mask.astype(bool), gsd=diff.res[0], area_size=10000)
