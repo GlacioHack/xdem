ed6fe78b28769a71778f3a68a74d2e8ccecbc8fe
Erik Holmlund <erik-holmlund@hotmail.com>
2021-04-14 13:56:39 +0200

    Made the examples fetch from the xdem-data repo instead of source (for longevity and much better performance)

--- xdem/examples.py
+++ xdem/examples.py
@@ -1,9 +1,9 @@
 """Utility functions to download and find example data."""
-import asyncio
 import os
-
-import geopandas as gpd
-import rasterio as rio
+import shutil
+import tarfile
+import tempfile
+import urllib.request
 
 EXAMPLES_DIRECTORY = os.path.abspath(os.path.join(os.path.dirname(__file__), "../", "examples/"))
 # Absolute filepaths to the example files.
@@ -18,64 +18,7 @@ FILEPATHS = {
         EXAMPLES_DIRECTORY,
         "Longyearbyen/data/glacier_mask/CryoClim_GAO_SJ_2010.shp"
     )
-
 }
-
-# The URLs for where to find the data.
-URLS = {
-    "longyearbyen_ref_dem": ("zip+https://publicdatasets.data.npolar.no/kartdata/S0_Terrengmodell/"
-                             "Mosaikk/NP_S0_DTM20.zip!NP_S0_DTM20/S0_DTM20.tif"),
-    "longyearbyen_tba_dem": ("zip+https://publicdatasets.data.npolar.no/kartdata/S0_Terrengmodell/"
-                             "Historisk/NP_S0_DTM20_199095_33.zip!NP_S0_DTM20_199095_33/S0_DTM20_199095_33.tif"),
-    "longyearbyen_glacier_outlines": "http://public.data.npolar.no/cryoclim/CryoClim_GAO_SJ_1990.zip",
-    "longyearbyen_glacier_outlines_2010": "https://public.data.npolar.no/cryoclim/CryoClim_GAO_SJ_2001-2010.zip"
-}
-
-
-async def _async_load_svalbard():
-    """Load the datasets asynchronously."""
-    # The bounding coordinates to crop the datasets.
-    bounds = {
-        "west": 502810,
-        "east": 529450,
-        "south": 8654330,
-        "north": 8674030
-    }
-
-    async def crop_dem(input_path, output_path, bounds):
-        """Read the input path and crop it to the given bounds."""
-        dem = rio.open(input_path)
-
-        upper, left = dem.index(bounds["west"], bounds["north"])
-        lower, right = dem.index(bounds["east"], bounds["south"])
-        window = rio.windows.Window.from_slices((upper, lower), (left, right))
-
-        data = dem.read(1, window=window)
-        meta = dem.meta.copy()
-        meta.update({
-            "transform": dem.window_transform(window),
-            "height": window.height,
-            "width": window.width
-        })
-        with rio.open(output_path, "w", **meta) as raster:
-            raster.write(data, 1)
-        print(f"Saved {output_path}")
-
-    async def read_outlines(input_path, output_path):
-        """Read outlines from a path and save them."""
-        outlines = gpd.read_file(input_path)
-        for col in outlines:
-            if outlines[col].dtype == "object":
-                outlines[col] = outlines[col].astype(str)
-        outlines.to_file(output_path)
-        print(f"Saved {output_path}")
-
-    await asyncio.gather(
-        crop_dem(URLS["longyearbyen_ref_dem"], FILEPATHS["longyearbyen_ref_dem"], bounds=bounds),
-        crop_dem(URLS["longyearbyen_tba_dem"], FILEPATHS["longyearbyen_tba_dem"], bounds=bounds),
-        read_outlines(URLS["longyearbyen_glacier_outlines"], FILEPATHS["longyearbyen_glacier_outlines"]),
-        read_outlines(URLS["longyearbyen_glacier_outlines_2010"], FILEPATHS["longyearbyen_glacier_outlines_2010"])
-    )
 
 
 def download_longyearbyen_examples(overwrite: bool = False):
@@ -87,7 +30,32 @@ def download_longyearbyen_examples(overwrite: bool = False):
     if not overwrite and all(map(os.path.isfile, list(FILEPATHS.values()))):
         print("Datasets exist")
         return
-    print("Downloading datasets from Longyearbyen")
-    os.makedirs(os.path.dirname(FILEPATHS["longyearbyen_glacier_outlines"]), exist_ok=True)
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(_async_load_svalbard())
+
+    # The URL from which to download the repository
+    url = "https://github.com/erikmannerfelt/xdem-data/tarball/main"
+
+    # Create a temporary directory to extract the tarball in.
+    temp_dir = tempfile.TemporaryDirectory()
+    tar_path = os.path.join(temp_dir.name, "data.tar.gz")
+
+    response = urllib.request.urlopen(url)
+    # If the response was right, download the tarball to the temporary directory
+    if response.getcode() == 200:
+        with open(tar_path, "wb") as outfile:
+            outfile.write(response.read())
+    else:
+        raise ValueError(f"Longyearbyen data fetch gave non-200 response: {response.status_code}")
+
+    # Extract the tarball
+    with tarfile.open(tar_path) as tar:
+        tar.extractall(temp_dir.name)
+
+    # Find the first directory in the temp_dir (should only be one) and construct the Longyearbyen data dir path.
+    dir_name = os.path.join(
+        temp_dir.name,
+        [dirname for dirname in os.listdir(temp_dir.name) if os.path.isdir(os.path.join(temp_dir.name, dirname))][0],
+        "data/Longyearbyen"
+    )
+
+    # Copy the data to the examples directory.
+    shutil.copytree(dir_name, os.path.join(EXAMPLES_DIRECTORY, "Longyearbyen/data"), dirs_exist_ok=True)
