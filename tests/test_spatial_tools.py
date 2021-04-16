bb943d00846383d86416fd03f1e1f3a0c27c7d6b
adehecq <3285905+adehecq@users.noreply.github.com>
2021-04-16 18:31:19 +0200

    Add tests for new merge_rasters options and fix issue with original tests

--- tests/test_spatial_tools.py
+++ tests/test_spatial_tools.py
@@ -31,25 +31,47 @@ def test_merge_rasters():
     x_midpoint -= x_midpoint % dem.res[0]
 
     # Cut the DEM into two DEMs that slightly overlap each other.
-    dem1 = dem.reproject(dst_bounds=rio.coords.BoundingBox(
+    dem1 = dem.copy()
+    dem1.crop(rio.coords.BoundingBox(
         right=x_midpoint + dem.res[0] * 3,
         left=dem.bounds.left,
         top=dem.bounds.top,
-        bottom=dem.bounds.bottom),
-        dst_crs=dem.crs
+        bottom=dem.bounds.bottom)
     )
-    dem2 = dem.reproject(dst_bounds=rio.coords.BoundingBox(
+    dem2 = dem.copy()
+    dem2.crop(rio.coords.BoundingBox(
         left=x_midpoint - dem.res[0] * 3,
         right=dem.bounds.right,
         top=dem.bounds.top,
-        bottom=dem.bounds.bottom),
-        dst_crs=dem.crs
+        bottom=dem.bounds.bottom)
     )
 
     # Merge the DEM and check that it closely resembles the initial DEM
     merged_dem = xdem.spatial_tools.merge_rasters([dem1, dem2])
     assert dem.data.shape == merged_dem.data.shape
+    assert dem.bounds == merged_dem.bounds
 
     diff = dem.data - merged_dem.data
 
     assert np.abs(np.nanmean(diff)) < 0.0001
+
+    # Check that include_ref works
+    merged_dem2 = xdem.spatial_tools.merge_rasters([dem, dem1, dem2], include_ref=False)
+    assert merged_dem2 == merged_dem
+
+    # Check that use_ref_bounds work - use a DEM that do not cover the whole extent
+    dem2 = dem.copy()
+    dem2.crop(rio.coords.BoundingBox(
+        left=x_midpoint - dem.res[0] * 3,
+        right=dem.bounds.right - dem.res[0]*2,
+        top=dem.bounds.top,
+        bottom=dem.bounds.bottom)
+    )
+
+    # This case should not preserve original extent
+    merged_dem = xdem.spatial_tools.merge_rasters([dem1, dem2])
+    assert merged_dem.bounds != dem.bounds
+
+    # This case should preserve original extent
+    merged_dem2 = xdem.spatial_tools.merge_rasters([dem, dem1, dem2], include_ref = False, use_ref_bounds=True)
+    assert merged_dem2.bounds == dem.bounds
