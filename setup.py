a7734debeb0570aa28bb075573957876a9de1b4a
Erik Mannerfelt <33550973+erikmannerfelt@users.noreply.github.com>
2021-05-17 13:15:26 +0200

    Improved documentation to explain why workarounds are needed.

--- setup.py
+++ setup.py
@@ -9,12 +9,19 @@ FULLVERSION = '0.0.2-2'
 VERSION = FULLVERSION
 
 class PostInstall(install):
-    """Post install script to install github dependencies."""
+    """
+    Post install script to install github dependencies.
 
+    The reason that this is needed is that PyPi has no functionality to parse github dependencies,
+    so this is a workaround until they fix it.
+
+    Inspired by: https://github.com/BaderLab/saber/issues/35#issuecomment-467827175
+    """
     pkgs = "git+https://github.com/GlacioHack/GeoUtils.git"
 
 
     def run(self):
+        """Install / reinstall each required GitHub package."""
         install.run(self)
 
         print(subprocess.getoutput(f"{sys.executable} -m pip install --force-reinstall {self.pkgs}"))
@@ -31,11 +38,11 @@ setup(name='xdem',
       long_description_content_type="text/markdown",
       url='https://github.com/GlacioHack/xdem',
       author='The GlacioHack Team',
-      author_email="john@doe.com",
+      author_email="john@doe.com",  # This is needed for PyPi unfortunately.
       license='BSD-3',
       packages=['xdem'],
       install_requires=['numpy', 'scipy', 'rasterio', 'geopandas',
-          'pyproj', 'tqdm', 'scikit-gstat', 'scikit-image'],
+                        'pyproj', 'tqdm', 'scikit-gstat', 'scikit-image'],
       extras_require={'rioxarray': ['rioxarray'], 'richdem': ['richdem'], 'pdal': [
           'pdal'], 'opencv': ['opencv'], "pytransform3d": ["pytransform3d"]},
       scripts=[],
