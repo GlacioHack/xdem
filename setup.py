905eb5e19c264542eb364228b5fd3135795e51b2
Erik Mannerfelt <33550973+erikmannerfelt@users.noreply.github.com>
2021-05-18 16:43:15 +0200

    Reverted 'hacky' geoutils solution because pip messed up the conda installation (specifically proj and proj-data)

--- setup.py
+++ setup.py
@@ -8,26 +8,6 @@ from setuptools.command.install import install
 FULLVERSION = '0.0.2-2'
 VERSION = FULLVERSION
 
-class PostInstall(install):
-    """
-    Post install script to install github dependencies.
-
-    The reason that this is needed is that PyPi has no functionality to parse github dependencies,
-    so this is a workaround until they fix it.
-
-    Inspired by: https://github.com/BaderLab/saber/issues/35#issuecomment-467827175
-    """
-    pkgs = "git+https://github.com/GlacioHack/GeoUtils.git"
-
-
-    def run(self):
-        """Install / reinstall each required GitHub package."""
-        install.run(self)
-
-        print(subprocess.getoutput(f"{sys.executable} -m pip install --force-reinstall {self.pkgs}"))
-
-
-
 with open(os.path.join(os.path.dirname(__file__), "README.md")) as infile:
     LONG_DESCRIPTION = infile.read()
 
@@ -38,15 +18,17 @@ setup(name='xdem',
       long_description_content_type="text/markdown",
       url='https://github.com/GlacioHack/xdem',
       author='The GlacioHack Team',
-      author_email="john@doe.com",  # This is needed for PyPi unfortunately.
+      author_email="this-is-not-an-email@a.com",  # This is needed for PyPi unfortunately.
       license='BSD-3',
       packages=['xdem'],
-      install_requires=['numpy', 'scipy', 'rasterio', 'geopandas',
-                        'pyproj', 'tqdm', 'scikit-gstat', 'scikit-image'],
+      install_requires=[
+          'numpy', 'scipy', 'rasterio', 'geopandas',
+          'pyproj', 'tqdm', 'scikit-gstat', 'scikit-image',
+          "geoutils @ https://github.com/GlacioHack/geoutils/tarball/master"
+      ],
       extras_require={'rioxarray': ['rioxarray'], 'richdem': ['richdem'], 'pdal': [
           'pdal'], 'opencv': ['opencv'], "pytransform3d": ["pytransform3d"]},
       scripts=[],
-      cmdclass={"install": PostInstall},
       zip_safe=False)
 
 write_version = True
