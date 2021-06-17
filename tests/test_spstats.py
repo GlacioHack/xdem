e73bcbfd49ceb2bfeda5ebf80f48867546025a07
rhugonne <romain.hugonnet@gmail.com>
2021-06-17 16:51:04 +0200

    add tests for nd binning function

--- tests/test_spstats.py
+++ tests/test_spstats.py
@@ -45,6 +45,29 @@ def load_diff() -> tuple[gu.georaster.Raster, np.ndarray]:
 
     return diff, mask
 
+def load_ref_and_diff() -> tuple[gu.georaster.Raster, gu.georaster.Raster, np.ndarray]:
+    """Load example files to try coregistration methods with."""
+    examples.download_longyearbyen_examples(overwrite=False)
+
+    reference_raster = gu.georaster.Raster(examples.FILEPATHS["longyearbyen_ref_dem"])
+    to_be_aligned_raster = gu.georaster.Raster(examples.FILEPATHS["longyearbyen_tba_dem"])
+    glacier_mask = gu.geovector.Vector(examples.FILEPATHS["longyearbyen_glacier_outlines"])
+    inlier_mask = ~glacier_mask.create_mask(reference_raster)
+
+    metadata = {}
+    # aligned_raster, _ = xdem.coreg.coregister(reference_raster, to_be_aligned_raster, method="amaury", mask=glacier_mask,
+    #                                          metadata=metadata)
+    nuth_kaab = xdem.coreg.NuthKaab()
+    nuth_kaab.fit(reference_raster.data, to_be_aligned_raster.data,
+                  inlier_mask=inlier_mask, transform=reference_raster.transform)
+    aligned_raster = nuth_kaab.apply(to_be_aligned_raster.data, transform=reference_raster.transform)
+
+    diff = gu.Raster.from_array((reference_raster.data - aligned_raster),
+                                transform=reference_raster.transform, crs=reference_raster.crs)
+    mask = glacier_mask.create_mask(diff)
+
+    return reference_raster, diff, mask
+
 
 class TestVariogram:
 
@@ -171,3 +194,30 @@ class TestPatchesMethod:
             gsd=diff.res[0],
             area_size=10000
         )
+
+class TestBinning:
+
+    def test_nd_binning(self):
+
+        ref, diff, mask = load_ref_and_diff()
+
+        slp, asp = xdem.coreg.calculate_slope_and_aspect(ref.data.squeeze())
+
+        # 1d binning
+        output = xdem.spstats.nd_binning_scipy(dh=diff.data.flatten(),list_var=[slp.flatten()],list_var_names=['slope'])
+
+        # nmad goes up quite a bit with slope, we can expect a 10 m measurement error difference
+        assert output[0][0].nmad_slope.values[-1] - output[0][0].nmad_slope.values[0] > 10
+
+        # try custom stat
+        def percentile_80(a):
+            return np.nanpercentile(a, 80)
+
+        xdem.spstats.nd_binning_scipy(dh=diff.data.flatten(),list_var=[slp.flatten()],list_var_names=['slope'], statistics=['count',percentile_80])
+
+        # 2d binning
+        output_2d = xdem.spstats.nd_binning_scipy(dh=diff.data.flatten(),list_var=[slp.flatten(),ref.data.flatten()],list_var_names=['slope','elevation'])
+
+        # nd binning
+        output_3d = xdem.spstats.nd_binning_scipy(dh=diff.data.flatten(),list_var=[slp.flatten(),ref.data.flatten(),asp.flatten()],list_var_names=['slope','elevation','aspect'])
+
