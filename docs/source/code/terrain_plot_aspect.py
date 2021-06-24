de8904160c63a5b2ee9285f6a0d9219a51369db1
Erik Mannerfelt <33550973+erikmannerfelt@users.noreply.github.com>
2021-06-24 09:18:18 +0200

    Changed to cyclic colormap.

--- docs/source/code/terrain_plot_aspect.py
+++ docs/source/code/terrain_plot_aspect.py
@@ -7,7 +7,7 @@ dem = xdem.DEM(xdem.examples.FILEPATHS["longyearbyen_ref_dem"])
 
 aspect = xdem.terrain.aspect(dem.data)
 
-plt.imshow(aspect.squeeze(), cmap="jet")
+plt.imshow(aspect.squeeze(), cmap="twilight")
 cbar = plt.colorbar()
 cbar.set_label("Aspect (°)")
 
