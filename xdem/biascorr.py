2c7931063ed2001787c9f369ab59d5b3546d6250
rhugonne <romain.hugonnet@gmail.com>
2021-06-26 20:40:24 +0200

    move ZScaleCorr to TerrainBias, wrap Along and Across into DirectionalBias

--- xdem/biascorr.py
+++ xdem/biascorr.py
@@ -9,87 +9,105 @@ import rasterio as rio
 import xdem
 
 
-class AlongTrack(xdem.coreg.Coreg):
+class DirectionalBias(xdem.coreg.Coreg):
     """
-    DEM along-track bias correction.
-
-    Estimates the mean (or median, weighted avg., etc.) offset between two DEMs.
+    For example for DEM along- or across-track bias correction.
     """
 
     def __init__(self, bias_func : Callable = xdem.robust_stats.robust_polynomial_fit):  # pylint: disable=super-init-not-called
         """
-        Instantiate an along-track correction object.
+        Instantiate an directional bias correction object.
 
-        :param bias_func: The function to use for calculating the bias. Default: robust polynomial of degree 1 to 6.
+        :param bias_func: The function to fit the bias. Default: robust polynomial of degree 1 to 6.
         """
         super().__init__(meta={"bias_func": bias_func})
 
     def _fit_func(self, ref_dem: np.ndarray, tba_dem: np.ndarray, transform: Optional[rio.transform.Affine],
-                  weights: Optional[np.ndarray], along_angle: Optional[float] = None, verbose: bool = False,**kwargs):
+                  weights: Optional[np.ndarray], angle: Optional[float] = None, verbose: bool = False,**kwargs):
         """Estimate the bias using the bias_func."""
 
         if verbose:
-            print('Getting along-track coordinates')
+            print('Getting directional coordinates')
 
         diff = ref_dem - tba_dem
-        xx, _ = xdem.spatial_tools.get_along(ref_dem,along_angle=along_angle)
+        x, _ = xdem.spatial_tools.get_rotated_xy(ref_dem,angle=angle)
 
         if verbose:
-            print("Estimating along-track bias correction with function "+ self.meta['bias_func'].__name__)
-        deg, coefs = self.meta["bias_func"](xx,diff,**kwargs)
+            print("Estimating directional bias correction with function "+ self.meta['bias_func'].__name__)
+        deg, coefs = self.meta["bias_func"](x,diff,**kwargs)
 
         if verbose:
-            print("Along-track bias estimated")
+            print("Directional bias estimated")
 
-        self._meta['deg'] = deg
+        self._meta['angle'] = angle
+        self._meta['degree'] = deg
         self._meta["coefs"] = coefs
 
     def _to_matrix_func(self) -> np.ndarray:
         """Convert the bias to a transform matrix."""
 
         raise ValueError(
-            "Along-track bias-corrections cannot be represented as transformation matrices.")
+            "Directional bias-corrections cannot be represented as transformation matrices.")
 
 
-class AcrossTrack(xdem.coreg.Coreg):
+class TerrainBias(xdem.coreg.Coreg):
     """
-    DEM bias correction.
+    Correct a bias according to terrain, such as elevation or curvature.
 
-    Estimates the mean (or median, weighted avg., etc.) offset between two DEMs.
+    With elevation: often useful for nadir image DEM correction, where the focal length is slightly miscalculated.
+    With curvature: often useful for a difference of DEMs with different effective resolution.
+
+    DISCLAIMER: An elevation correction may introduce error when correcting non-photogrammetric biases, as generally
+    elevation biases are interlinked with curvature biases.
+    See Gardelle et al. (2012) (Figure 2), http://dx.doi.org/10.3189/2012jog11j175, for curvature-related biases.
     """
 
-    def __init__(self,
-                 bias_func: Callable = xdem.robust_stats.robust_polynomial_fit):  # pylint: disable=super-init-not-called
+    def __init__(self, bias_func = xdem.robust_stats.robust_polynomial_fit):
         """
-        Instantiate an across-track correction object.
+        Instantiate an terrain bias correction object
 
-        :param bias_func: The function to use for calculating the bias. Default: robust polynomial of degree 1 to 6.
+        :param bias_func: The function to fit the bias. Default: robust polynomial of degree 1 to 6.
         """
         super().__init__(meta={"bias_func": bias_func})
 
-    def _fit_func(self, ref_dem: np.ndarray, tba_dem: np.ndarray, transform: Optional[rio.transform.Affine],
-                  weights: Optional[np.ndarray], along_angle: Optional[float] = None, verbose: bool = False, **kwargs):
+    def _fit_func(self, ref_dem: np.ndarray, tba_dem: np.ndarray, attribute: np.ndarray,
+                  transform: Optional[rio.transform.Affine], weights: Optional[np.ndarray], verbose: bool = False,
+                  **kwargs):
         """Estimate the bias using the bias_func."""
 
-        if verbose:
-            print('Getting across-track coordinates')
-
         diff = ref_dem - tba_dem
-        _, yy = xdem.spatial_tools.get_along(ref_dem, along_angle=along_angle)
 
         if verbose:
-            print("Estimating across-track bias correction with function " + self.meta['bias_func'].__name__)
-        deg, coefs = self.meta["bias_func"](yy, diff, **kwargs)
+            print("Estimating terrain bias correction with function " + self.meta['bias_func'].__name__)
+        deg, coefs = self.meta["bias_func"](attribute, diff, **kwargs)
 
         if verbose:
-            print("Across-track bias estimated")
+            print("Terrain bias estimated")
 
-        self._meta['deg'] = deg
-        self._meta["coefs"] = coefs
+        self._meta['degree'] = deg
+        self._meta['coefs'] = coefs
+
+    def _apply_func(self, dem: np.ndarray, transform: rio.transform.Affine) -> np.ndarray:
+        """Apply the scaling model to a DEM."""
+        model = np.poly1d(self._meta['coefs'])
+
+        return dem + model(dem)
+
+    def _apply_pts_func(self, coords: np.ndarray) -> np.ndarray:
+        """Apply the scaling model to a set of points."""
+        model = np.poly1d(self._meta['coefs'])
+
+        new_coords = coords.copy()
+        new_coords[:, 2] += model(new_coords[:, 2])
+        return new_coords
 
     def _to_matrix_func(self) -> np.ndarray:
-        """Convert the bias to a transform matrix."""
+        """Convert the transform to a matrix, if possible."""
+        if self.degree == 0:  # If it's just a bias correction.
+            return self._meta["coefficients"][-1]
+        elif self.degree < 2:
+            raise NotImplementedError
+        else:
+            raise ValueError("A 2nd degree or higher ZScaleCorr cannot be described as a 4x4 matrix!")
 
-        raise ValueError(
-            "Across-track bias-corrections cannot be represented as transformation matrices.")
 
