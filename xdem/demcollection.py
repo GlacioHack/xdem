Fix `load=False` bug with DEM class + silent option `True` as default (#166)
* fix bug + silent option true as default

* Fix with geoutils update

* fix demcollection and remove silent flags

xdem/demcollection.py
@@ -47,13 +47,14 @@ class DEMCollection:
 
         # Find the sort indices from the timestamps
         indices = np.argsort(self.timestamps.astype("int64"))
-        self.dems = np.asarray(dems)[indices]
+        self.dems = np.empty(len(dems), dtype=object)
+        self.dems[:] = [dems[i] for i in indices]
         self.ddems: list[xdem.dDEM] = []
         # The reference index changes place when sorted
         if isinstance(reference_dem, int):
             self.reference_index = np.argwhere(indices == reference_dem)[0][0]
         elif isinstance(reference_dem, gu.georaster.Raster):
-            self.reference_index = np.argwhere(self.dems == reference_dem)[0][0]
+            self.reference_index = [i for i, dem in enumerate(self.dems) if dem is reference_dem][0]
 
         if outlines is None:
             self.outlines: dict[np.datetime64, gu.geovector.Vector] = {}