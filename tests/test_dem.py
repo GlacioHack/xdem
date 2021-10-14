f35fecf9fc3948c6e3cad83ffd0935affee1efe6
rhugonne <romain.hugonnet@gmail.com>
2021-10-14 11:10:09 +0200

    streamline comments, doc

--- tests/test_dem.py
+++ tests/test_dem.py
@@ -77,11 +77,12 @@ class TestDEM:
         # Check the object is a DEM
         assert isinstance(r2, xdem.dem.DEM)
 
-        # check all immutable attributes are equal
+        # Check all immutable attributes are equal
         # georaster_attrs = ['bounds', 'count', 'crs', 'dtypes', 'height', 'indexes', 'nodata',
         #                    'res', 'shape', 'transform', 'width']
         # satimg_attrs = ['satellite', 'sensor', 'product', 'version', 'tile_name', 'datetime']
         # dem_attrs = ['vref', 'vref_grid', 'ccrs']
+
         # using list directly available in Class
         attrs = [at for at in r._get_rio_attrs() if at not in ['name', 'dataset_mask', 'driver']]
         all_attrs = attrs + si.satimg_attrs + xdem.dem.dem_attrs
@@ -99,43 +100,44 @@ class TestDEM:
         assert not np.array_equal(r.data, r2.data, equal_nan=True)
 
     def test_set_vref(self):
+        """Tests to set the vertical reference"""
 
         fn_img = xdem.examples.get_path("longyearbyen_ref_dem")
         img = DEM(fn_img)
 
-        # check for WGS84
+        # Check setting WGS84
         img.set_vref(vref_name='WGS84')
         assert img.vref == 'WGS84'
         assert img.vref_grid is None
 
-        # check for EGM96
+        # Check setting EGM96
         img.set_vref(vref_name='EGM96')
         assert img.vref == 'EGM96'
         assert img.vref_grid == 'us_nga_egm96_15.tif'
-        # grid should have priority over name and parse the right vref name
+        # The grid argument should have priority over name and parse the right vref name
         img.set_vref(vref_name='WGS84', vref_grid='us_nga_egm96_15.tif')
         assert img.vref == 'EGM96'
 
-        # check for EGM08
+        # Check setting EGM08
         img.set_vref(vref_name='EGM08')
         assert img.vref == 'EGM08'
         assert img.vref_grid == 'us_nga_egm08_25.tif'
-        # grid should have priority over name and parse the right vref name
+        # The grid argument should have priority over name and parse the right vref name
         img.set_vref(vref_name='best ref in the entire world, or any string', vref_grid='us_nga_egm08_25.tif')
         assert img.vref == 'EGM08'
 
-        # check that other existing grids are well detected in the pyproj.datadir
+        # Check that other existing grids are well detected in the pyproj.datadir
         img.set_vref(vref_grid='is_lmi_Icegeoid_ISN93.tif')
 
-        # check that non-existing grids raise errors
+        # Check that non-existing grids raise errors
         with pytest.raises(ValueError):
             img.set_vref(vref_grid='the best grid in the entire world, or any non-existing string')
 
     def test_to_vref(self):
 
-        # first, some points to test the transform
+        # First, use test points to test the vertical transform
 
-        # Chile
+        # In Chile
         lat = 43.70012234
         lng = -79.41629234
         z = 100
@@ -148,22 +150,21 @@ class TestDEM:
         transformer = pyproj.Transformer.from_proj(ellipsoid, geoid)
         z_out = transformer.transform(lng, lat, z)[2]
 
-        # check final elevation is finite, higher than ellipsoid with less than 40 m difference (typical geoid in Chile)
+        # Check that the final elevation is finite, and higher than ellipsoid by less than 40 m (typical geoid in Chile)
         assert np.logical_and.reduce((np.isfinite(z_out), np.greater(z_out, z), np.less(np.abs(z_out-z), 40)))
 
-        # egm2008
+        # With the EGM2008 (catch warnings as this use of init is depecrated)
         with warnings.catch_warnings():
             warnings.filterwarnings("ignore", module="pyproj")
-            # init is deprecated by
             ellipsoid = pyproj.Proj(init="EPSG:4326")  # WGS84 datum ellipsoid height
             geoid = pyproj.Proj(init="EPSG:4326", geoidgrids='us_nga_egm08_25.tif')
         transformer = pyproj.Transformer.from_proj(ellipsoid, geoid)
         z_out = transformer.transform(lng, lat, z)[2]
 
-        # check final elevation is finite, higher than ellipsoid with less than 40 m difference (typical geoid in Chile)
+        # Check final elevation is finite, higher than ellipsoid with less than 40 m difference (typical geoid in Chile)
         assert np.logical_and.reduce((np.isfinite(z_out), np.greater(z_out, z), np.less(np.abs(z_out-z), 40)))
 
-        # geoid2006 for Alaska
+        # With GEOID2006 for Alaska
         lat = 65
         lng = -140
         with warnings.catch_warnings():
@@ -174,10 +175,10 @@ class TestDEM:
         transformer = pyproj.Transformer.from_proj(ellipsoid, geoid)
         z_out = transformer.transform(lng, lat, z)[2]
 
-        # check final elevation is finite, lower than ellipsoid with less than 20 m difference (typical geoid in Alaska)
+        # Check that the final elevation is finite, lower than ellipsoid by less than 20 m (typical geoid in Alaska)
         assert np.logical_and.reduce((np.isfinite(z_out), np.less(z_out, z), np.less(np.abs(z_out-z), 20)))
 
-        # isn1993 for Iceland
+        # With ISN1993 for Iceland
         lat = 65
         lng = -18
         with warnings.catch_warnings():
@@ -189,16 +190,16 @@ class TestDEM:
         transformer = pyproj.Transformer.from_proj(ellipsoid, geoid)
         z_out = transformer.transform(lng, lat, z)[2]
 
-        # check final elevation is finite, lower than ellipsoid with less than 100 m difference (typical geoid in Iceland)
+        # Check that the final elevation is finite, lower than ellipsoid by less than 100 m (typical geoid in Iceland)
         assert np.logical_and.reduce((np.isfinite(z_out), np.less(z_out, z), np.less(np.abs(z_out-z), 100)))
 
-        # checking that the function does not run without a reference set
+        # Check that the function does not run without a reference set
         fn_img = xdem.examples.get_path("longyearbyen_ref_dem")
         img = DEM(fn_img)
         with pytest.raises(ValueError):
             img.to_vref(vref_name='EGM96')
 
-        # checking that the function properly runs with a reference set
+        # Check that the function properly runs with a reference set
         img.set_vref(vref_name='WGS84')
         mean_ellips = np.nanmean(img.data)
         img.to_vref(vref_name='EGM96')
@@ -207,7 +208,6 @@ class TestDEM:
         assert img.vref == 'EGM96'
         assert img.vref_grid == 'us_nga_egm96_15.tif'
 
-        # check that the geoid is lower than ellipsoid, less than 35 m difference (Svalbard)
-
+        # Check that the geoid is lower than ellipsoid, less than 35 m difference (Svalbard)
         assert np.greater(mean_ellips, mean_geoid_96)
         assert np.less(np.abs(mean_ellips-mean_geoid_96), 35.)
