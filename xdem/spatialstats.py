0c699ee658bb9db874e27f3b6713dc322f028aca
Romain Hugonnet <romain.hugonnet@gmail.com>
2021-10-15 10:54:07 +0200

    Add scaling factor details to `nmad` function description (#221)
    * fix deramp residual and optimizer
    
    * add details on nmad scaling factor within function description
    
    * add link to wiki mad page

--- xdem/spatialstats.py
+++ xdem/spatialstats.py
@@ -32,9 +32,12 @@ with warnings.catch_warnings():
 def nmad(data: np.ndarray, nfact: float = 1.4826) -> float:
     """
     Calculate the normalized median absolute deviation (NMAD) of an array.
+    Default scaling factor is 1.4826 to scale the median absolute deviation (MAD) to the dispersion of a normal
+    distribution (see https://en.wikipedia.org/wiki/Median_absolute_deviation#Relation_to_standard_deviation, and
+    e.g. http://dx.doi.org/10.1016/j.isprsjprs.2009.02.003)
 
     :param data: input data
-    :param nfact: normalization factor for the data; default is 1.4826
+    :param nfact: normalization factor for the data
 
     :returns nmad: (normalized) median absolute deviation of data.
     """
