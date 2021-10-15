5357d4e3e7a05e54afde917b33a16f6e1afecde7
Andrew Tedstone <andrew.tedstone@unifr.ch>
2021-10-15 08:27:11 +0200

    Remove spatial_tools from xdem. Now in geoutils.

--- xdem/__init__.py
+++ xdem/__init__.py
@@ -1,4 +1,4 @@
-from . import coreg, dem, examples, spatial_tools, spatialstats, volume, filters, fit, terrain
+from . import coreg, dem, examples, spatialstats, volume, filters, fit, terrain
 from .ddem import dDEM
 from .dem import DEM
 from .demcollection import DEMCollection
