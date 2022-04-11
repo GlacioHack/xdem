f14edc8be76f788950ba6cb1e4d0a3e3e17612c0
rhugonne <romain.hugonnet@gmail.com>
2022-04-11 13:53:43 +0200

    Update value of patches method test with new NaNs of dataset diff

--- tests/test_spatialstats.py
+++ tests/test_spatialstats.py
@@ -280,7 +280,7 @@ class TestPatchesMethod:
 
         # Check the sampling is always fixed for a random state
         assert df['tile'].values[0] == '31_184'
-        assert df['nanmedian'].values[0] == pytest.approx(2.3, abs=0.01)
+        assert df['nanmedian'].values[0] == pytest.approx(2.28, abs=0.01)
 
         # Check that all counts respect the default minimum percentage of 80% valid pixels
         assert all(df['count'].values > 0.8*np.max(df['count'].values))
