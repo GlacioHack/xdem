6a264db9e4a39d4880ebbffc08a6b8ac9a5da441
adehecq <3285905+adehecq@users.noreply.github.com>
2022-05-20 18:32:42 +0200

    Update plot_nuth_kaab example

--- examples/plot_nuth_kaab.py
+++ examples/plot_nuth_kaab.py
@@ -8,8 +8,7 @@ In ``xdem``, this approach is implemented through the :class:`xdem.coreg.NuthKaa
 For more information about the approach, see :ref:`coregistration_nuthkaab`.
 """
 import geoutils as gu
-import matplotlib.pyplot as plt
-
+import numpy as np
 import xdem
 
 # %%
@@ -21,19 +20,13 @@ glacier_outlines = gu.Vector(xdem.examples.get_path("longyearbyen_glacier_outlin
 # Create a stable ground mask (not glacierized) to mark "inlier data"
 inlier_mask = ~glacier_outlines.create_mask(reference_dem)
 
-plt_extent = [
-    reference_dem.bounds.left,
-    reference_dem.bounds.right,
-    reference_dem.bounds.bottom,
-    reference_dem.bounds.top,
-]
 
 # %%
 # The DEM to be aligned (a 1990 photogrammetry-derived DEM) has some vertical and horizontal biases that we want to avoid.
 # These can be visualized by plotting a change map:
 
 diff_before = reference_dem - dem_to_be_aligned
-diff_before.show(cmap="coolwarm_r", vmin=-10, vmax=10)
+diff_before.show(cmap="coolwarm_r", vmin=-10, vmax=10, cb_title="Elevation change (m)")
 
 
 # %%
@@ -50,14 +43,19 @@ aligned_dem = nuth_kaab.apply(dem_to_be_aligned)
 # Then, the new difference can be plotted to validate that it improved.
 
 diff_after = reference_dem - aligned_dem
-diff_after.show(cmap="coolwarm_r", vmin=-10, vmax=10)
+diff_after.show(cmap="coolwarm_r", vmin=-10, vmax=10, cb_title="Elevation change (m)")
 
 
 # %%
-# We compare the NMAD to validate numerically that there was an improvement (see :ref:`robuststats_meanstd`):
+# We compare the median and NMAD to validate numerically that there was an improvement (see :ref:`robuststats_meanstd`):
+inliers_before = diff_before.data[inlier_mask].compressed()
+med_before, nmad_before = np.median(inliers_before), xdem.spatialstats.nmad(inliers_before)
 
-print(f"Error before: {xdem.spatialstats.nmad(diff_before):.2f} m")
-print(f"Error after: {xdem.spatialstats.nmad(diff_after):.2f} m")
+inliers_after = diff_after.data[inlier_mask].compressed()
+med_after, nmad_after = np.median(inliers_after), xdem.spatialstats.nmad(inliers_after)
+
+print(f"Error before: median = {med_before:.2f} - NMAD = {nmad_before:.2f} m")
+print(f"Error after: median = {med_after:.2f} - NMAD = {nmad_after:.2f} m")
 
 # %%
 # In the plot above, one may notice a positive (blue) tendency toward the east.
