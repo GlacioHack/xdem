413aae8b4b1a1bef0067c0a6f1f34a721f99aa5d
rhugonne <romain.hugonnet@gmail.com>
2022-08-30 10:35:05 +0200

    Update code snippets with easier syntax

--- docs/source/code/spatialstats.py
+++ docs/source/code/spatialstats.py
@@ -9,20 +9,20 @@ dh = gu.georaster.Raster(xdem.examples.get_path("longyearbyen_ddem"))
 ref_dem = xdem.DEM(xdem.examples.get_path("longyearbyen_ref_dem"))
 glacier_mask = gu.geovector.Vector(xdem.examples.get_path("longyearbyen_glacier_outlines"))
 mask = glacier_mask.create_mask(dh)
-slope = xdem.terrain.get_terrain_attribute(ref_dem.data, resolution=ref_dem.res[0], attribute=['slope'])
+slope = xdem.terrain.get_terrain_attribute(ref_dem, attribute=['slope'])
 
 # Keep only stable terrain data
-dh.data[mask] = np.nan
+dh.set_mask(mask)
 
 # Estimate the measurement error by bin of slope, using the NMAD as robust estimator
-df_ns = xdem.spatialstats.nd_binning(dh.data.ravel(), list_var=[slope.ravel()], list_var_names=['slope'],
+df_ns = xdem.spatialstats.nd_binning(dh.data.ravel(), list_var=[slope.data.ravel()], list_var_names=['slope'],
                                      statistics=['count', xdem.spatialstats.nmad])
 
 # Derive a numerical function of the measurement error
 err_dh = xdem.spatialstats.interp_nd_binning(df_ns, list_var_names=['slope'])
 
 # Standardize the data
-z_dh = dh.data.ravel() / err_dh(slope.ravel())
+z_dh = dh.data.ravel() / err_dh(slope.data.ravel())
 
 # Sample empirical variogram
 df_vgm = xdem.spatialstats.sample_empirical_variogram(values=dh.data, gsd=dh.res[0], subsample=50, random_state=42)
