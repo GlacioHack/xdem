7acab860d22cb0cdae2d31d68b5a98bf5fd88084
rhugonne <romain.hugonnet@gmail.com>
2022-09-18 16:47:26 +0200

    Add check for base environment in CI and test that .yml files are consistent

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -8,6 +8,46 @@ import pytest
 import xdem
 import xdem.misc
 
+from __future__ import annotations
+
+import os
+
+import yaml  # type: ignore
+
+
+class TestMisc:
+    def test_environment_files(self) -> None:
+        """Check that environment yml files are properly written: all dependencies of env are also in dev-env"""
+
+        fn_env = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "environment.yml"))
+        fn_devenv = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "dev-environment.yml"))
+
+        # Load the yml as dictionaries
+        yaml_env = yaml.safe_load(open(fn_env))
+        yaml_devenv = yaml.safe_load(open(fn_devenv))
+
+        # Extract the dependencies values
+        conda_dep_env = yaml_env["dependencies"]
+        conda_dep_devenv = yaml_devenv["dependencies"]
+
+        # Check if there is any pip dependency, if yes pop it from the end of the list
+        if isinstance(conda_dep_devenv[-1], dict):
+            pip_dep_devenv = conda_dep_devenv.pop()
+
+            # Check if there is a pip dependency in the normal env as well, if yes pop it also
+            if isinstance(conda_dep_env[-1], dict):
+                pip_dep_env = conda_dep_env.pop()
+
+                # The diff below computes the dependencies that are in env but not in dev-env
+                # It should be empty, otherwise we raise an error
+                diff_pip_check = list(set(pip_dep_env) - set(pip_dep_devenv))
+                assert len(diff_pip_check) == 0
+
+        # We do the same for the conda dependency, first a sanity check that everything that is in env is also in dev-ev
+        diff_conda_check = list(set(conda_dep_env) - set(conda_dep_devenv))
+        assert len(diff_conda_check) == 0
+
+
 
 @pytest.mark.parametrize("deprecation_increment", [-1, 0, 1, None])
 @pytest.mark.parametrize("details", [None, "It was completely useless!", "dunnowhy"])
