7c1068c326eb011a22a46b68f0a017ce2d6d7f6b
rhugonne <romain.hugonnet@gmail.com>
2022-09-20 10:05:53 +0200

    Change typing with Python version

--- xdem/_typing.py
+++ xdem/_typing.py
@@ -1,14 +1,18 @@
 from __future__ import annotations
 
+import sys
 from typing import Any
 
 import numpy as np
 
-try:
+# Only for Python >= 3.9
+if sys.version_info.minor >= 9:
+
     from numpy.typing import NDArray  # this syntax works starting on Python 3.9
 
     NDArrayf = NDArray[np.floating[Any]]  # type: ignore
-except ImportError:
+
+else:
     NDArrayf = np.array[Any, np.dtype[np.floating[Any]]]  # type: ignore
 
 MArrayf = np.ma.masked_array[Any, np.dtype[np.floating[Any]]]
