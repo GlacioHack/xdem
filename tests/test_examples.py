ecc7a9ce966cbd6f1c0dfcffa8e75cd2db25cea7
Romain Hugonnet <romain.hugonnet@gmail.com>
2022-10-25 14:02:47 +0200

    Get consistent `scipy.optimize` outputs out of `NuthKaab` (#322)
    * Try higher round up to fix test on Mac
    
    * Add back Mac to test floating values
    
    * Print results to see before rounding
    
    * Update fixed tested values with new rounding
    
    * Lower tolerance
    
    * Update example
    
    * Add test on example co-registration to check where OS difference come from
    
    * Linting
    
    * Try to lower tolerance even more
    
    * Reup tolerance
    
    * Try rounding more
    
    * Try with new values
    
    * Fix comma separator
    
    * Fix values in patches tests
    
    * Linting
    
    * Remove unused changes

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,8 +1,6 @@
 """Functions to test the example data."""
 from __future__ import annotations
 
-import platform
-
 import geoutils as gu
 import numpy as np
 import pytest
@@ -36,11 +34,11 @@ class TestExamples:
                 ddem,
                 np.array(
                     [
-                        -2.423095703125000000e-02,
-                        -7.189941406250000000e-01,
-                        1.425781250000000000e-01,
-                        1.101867675781250000e00,
-                        -5.920959472656250000e00,
+                        -4.669189453125000000e-02,
+                        -7.413940429687500000e-01,
+                        1.499481201171875000e-01,
+                        1.095550537109375000e00,
+                        -5.904846191406250000e00,
                     ],
                     dtype=np.float32,
                 ),
@@ -50,14 +48,12 @@ class TestExamples:
     def test_array_content(self, rst_and_truevals: tuple[Raster, NDArrayf]) -> None:
         """Let's ensure the data arrays in the examples are always the same by checking randomly some values"""
 
-        # TODO: this currently fails on Mac while exactly the same on Linux and Windows... why?
-        if platform.system() in ["Linux", "Windows"]:
-            rst = rst_and_truevals[0]
-            truevals = rst_and_truevals[1]
-            np.random.seed(42)
-            values = np.random.choice(rst.data.data.flatten(), size=5, replace=False)
+        rst = rst_and_truevals[0]
+        truevals = rst_and_truevals[1]
+        np.random.seed(42)
+        values = np.random.choice(rst.data.data.flatten(), size=5, replace=False)
 
-            assert values == pytest.approx(truevals)
+        assert values == pytest.approx(truevals)
 
     @pytest.mark.parametrize("rst_and_truenodata", [(ref_dem, 0), (tba_dem, 0), (ddem, 2316)])  # type: ignore
     def test_array_nodata(self, rst_and_truenodata: tuple[Raster, int]) -> None:
