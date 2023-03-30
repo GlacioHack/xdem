662037fc858ed2436e41fec17a74254b329c1ae4
Shubham Jadhav <shubham@Shubhams-MacBook-Air.local>
2023-03-30 19:54:24 +0530

    changed geoutils to xdem in import error message

--- xdem/__init__.py
+++ xdem/__init__.py
@@ -16,7 +16,7 @@ try:
     from xdem.version import version as __version__  # noqa
 except ImportError:  # pragma: no cover
     raise ImportError(
-        "geoutils is not properly installed. If you are "
+        "xdem is not properly installed. If you are "
         "running from the source directory, please instead "
         "create a new virtual environment (using conda or "
         "virtualenv) and then install it in-place by running: "
