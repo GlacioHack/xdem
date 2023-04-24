5223adcf629d565831d764507dd86fc4bd3ef7af
Romain Hugonnet <romain.hugonnet@gmail.com>
2023-04-24 15:55:29 -0700

    Add dark logo and favicon (#365)

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -164,6 +164,9 @@ html_theme_options = {
         # For launching Binder in Jupyterlab to open MD files as notebook (downloads them otherwise)
     },
     "show_toc_level": 2,  # To show more levels on the right sidebar TOC
+    "logo": {
+        "image_dark": "_static/xdem_logo_dark.svg",
+    },
 }
 
 # For dark mode
