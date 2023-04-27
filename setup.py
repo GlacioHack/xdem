c80ef0d042d48a1c588c31abbf0359d2c9af3808
Romain Hugonnet <romain.hugonnet@gmail.com>
2023-04-26 17:57:02 -0700

    Change version to `0.0.9`

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 
 from setuptools import setup
 
-FULLVERSION = "0.0.8"
+FULLVERSION = "0.0.9"
 VERSION = FULLVERSION
 
 with open(os.path.join(os.path.dirname(__file__), "README.md")) as infile:
