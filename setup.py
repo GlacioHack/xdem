adb753b26552954b70d64e3c62e664729ecabb03
Romain Hugonnet <romain.hugonnet@gmail.com>
2023-09-03 16:52:23 -0800

    Update setup.py to remain only for backw-compat

--- setup.py
+++ setup.py
@@ -1,28 +1,4 @@
-from __future__ import annotations
-
-import os
-
+"""This file now only serves for backward-compatibility for routines explicitly calling python setup.py"""
 from setuptools import setup
 
 setup()
-
-write_version = True
-
-
-def write_version_py(filename: str | None = None) -> None:
-    cnt = """\
-version = '%s'
-short_version = '%s'
-"""
-    if not filename:
-        filename = os.path.join(os.path.dirname(__file__), "xdem", "__version__.py")
-
-    a = open(filename, "w")
-    try:
-        a.write(cnt % (FULLVERSION, VERSION))
-    finally:
-        a.close()
-
-
-if write_version:
-    write_version_py()
