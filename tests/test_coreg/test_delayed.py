afcacf02324038a4d3093773e61a72a54e9deccc
Romain Hugonnet <romain.hugonnet@gmail.com>
2024-04-22 14:48:15 -0800

    Fix little issues and add tests

--- tests/test_coreg/test_delayed.py
+++ tests/test_coreg/test_delayed.py
@@ -1,4 +1,72 @@
 """Tests for dask functions (temporarily in xDEM, might move to GeoUtils)."""
+import os
+
+import numpy as np
+import pytest
+import xarray as xr
+
+from xdem.examples import _EXAMPLES_DIRECTORY
+from xdem.coreg.delayed import delayed_subsample, delayed_interp_points, _random_state_from_user_input
 
 class TestDelayed:
-    pass
+
+    # Create test file (replace with data in time?)
+    fn_tmp = os.path.join(_EXAMPLES_DIRECTORY, "test.nc")
+    if not os.path.exists(fn_tmp):
+        data = np.random.normal(size=400000000).reshape(20000, 20000)
+        da = xr.DataArray(data=data, dims=["x", "y"])
+        ds = xr.Dataset(data_vars={"test": da})
+        encoding_kwargs = {"test": {"chunksizes": (100, 100)}}
+        ds.to_netcdf(fn_tmp, encoding=encoding_kwargs)
+        del ds, da, data
+
+    # Chunk size in memory
+    chunksize = 500
+
+    @pytest.mark.parametrize("subsample_size", [2, 100, 100000])
+    def test_delayed_subsample(self, subsample_size: int):
+        """Checks for delayed subsampling function."""
+
+        # Open dataset with chunks
+        ds = xr.open_dataset(self.fn_tmp, chunks={"x": self.chunksize, "y": self.chunksize})
+        darr = ds["test"].data
+
+        # Derive subsample from delayed function
+        sub = delayed_subsample(darr, subsample=subsample_size, random_state=42)
+
+        # The subsample should have exactly the prescribed length, with only valid values
+        assert len(sub) == subsample_size
+        assert all(np.isfinite(sub))
+
+        # To verify the sampling works correctly, we can get its subsample indices with the argument return_indices
+        # And compare to the same subsample with vindex (now that we know the coordinates of valid values sampled)
+        indices = delayed_subsample(darr, subsample=subsample_size, random_state=42, return_indices=True)
+        sub2 = np.array(darr.vindex[indices[0], indices[1]])
+        assert np.array_equal(sub, sub2)
+
+    @pytest.mark.parametrize("ninterp", [2, 100, 100000])
+    def test_delayed_interp_points(self, ninterp: int):
+        """Checks for delayed interpolate points function."""
+
+        # Open dataset with chunks
+        ds = xr.open_dataset(self.fn_tmp, chunks={"x": self.chunksize, "y": self.chunksize})
+        darr = ds["test"].data
+
+        # Create random point coordinates to interpolate
+        rng = np.random.default_rng(seed=42)
+        interp_x = rng.choice(ds.x.size, ninterp) + rng.random(ninterp)
+        interp_y = rng.choice(ds.y.size, ninterp) + rng.random(ninterp)
+
+        # Interpolate with delayed function
+        interp1 = delayed_interp_points(darr=darr, points=(interp_x, interp_y), resolution=(1, 1))
+
+        # Interpolate directly with Xarray and compare results are the same
+        xx = xr.DataArray(interp_x, dims='z', name='x')
+        yy = xr.DataArray(interp_y, dims='z', name='y')
+        interp2 = ds.test.interp(x=xx, y=yy)
+        interp2.compute()
+        interp2 = np.array(interp2.values)
+
+        assert np.array_equal(interp1, interp2, equal_nan=True)
+
+
--- xdem/coreg/delayed.py
+++ xdem/coreg/delayed.py
@@ -1,5 +1,5 @@
 """
-Temporary module for delayed functions used in coregistration,
+Temporary module for Dask delayed functions for out-of-memory coregistration,
 most should be probably be moved to GeoUtils/array and integrated into the GeoUtils Xarray accessor
 
 See xDEM discussion thread: https://github.com/GlacioHack/xdem/discussions/501
@@ -30,7 +30,7 @@ def _random_state_from_user_input(random_state: np.random.RandomState | int | No
     elif isinstance(random_state, np.random.RandomState):
         rnd = random_state
     else:
-        rnd = np.random.RandomState(np.random.MT19937(np.random.SeedSequence(random_state)))
+        rnd = np.random.default_rng(seed=42)
 
     return rnd
 
@@ -51,9 +51,9 @@ def _get_subsample_size_from_user_input(subsample: int | float, total_valids: in
     return npoints
 
 def _get_indices_block_per_subsample(xxs: np.ndarray, num_chunks: tuple[int, int], list_valids: list[int]) -> list[list[int]]:
-    """Get indices of block where each 1D subsample belongs."""
+    """Get list of block-relative 1D subsample indexes for each block."""
 
-    # Apply a cumulative sum to valids to get their first 1D total index
+    # Apply a cumulative sum to get the first 1D total index of each block
     valids_cumsum = np.cumsum(list_valids)
 
     # We can write a faster algorithm by sorting
@@ -61,17 +61,24 @@ def _get_indices_block_per_subsample(xxs: np.ndarray, num_chunks: tuple[int, int
 
     # We define a list of indices bucket per block.
     ind_buckets = [[] for _ in range((num_chunks[0] * num_chunks[1]))]
-    k = 0
-    for i, x in enumerate(xxs):
+    k = 0  # K is the block number
+    for x in xxs:
 
-        # Move to the next block(s) where current index is, if not in this one
-        while xxs > valids_cumsum[k]:
+        # Move to the next block K where current 1D subsample index is, if not in this one
+        while x >= valids_cumsum[k]:
             k += 1
 
-        # Add index ID to block
-        ind_buckets[k].append(i)
+        # Add 1D subsample index  relative to first subsample index of this block
+        first_xindex_block = valids_cumsum[k - 1] if k >= 1 else 0  # The first 1D valid subsample index of the block
+        relative_xindex = x - first_xindex_block
+        ind_buckets[k].append(relative_xindex)
 
     return ind_buckets
+
+@dask.delayed
+def _delayed_nb_valids(arr_chunk: np.ndarray) -> np.ndarray:
+    """Count number of valid values per block."""
+    return np.array([np.count_nonzero(np.isfinite(arr_chunk))]).reshape((1, 1))
 
 @dask.delayed
 def _delayed_subsample_block(arr_chunk: np.ndarray, subsample_indices: np.ndarray) -> np.ndarray:
@@ -81,10 +88,23 @@ def _delayed_subsample_block(arr_chunk: np.ndarray, subsample_indices: np.ndarra
 
     return s_chunk
 
+@dask.delayed
+def _delayed_subsample_indices_block(arr_chunk: np.ndarray, subsample_indices: np.ndarray, block_id: dict[str, Any]) -> np.ndarray:
+    """Return 2D indices at the subsampled 1D indices per block."""
+
+    # Unravel indices of valid data to the shape of the block
+    ix, iy = np.unravel_index(np.argwhere(np.isfinite(arr_chunk.flatten()))[subsample_indices], shape=arr_chunk.shape)
+
+    # Convert to full-array indexes by adding the row and column starting indexes for this block
+    ix += block_id["xstart"]
+    iy += block_id["ystart"]
+
+    return np.hstack((ix, iy))
+
 def delayed_subsample(darr: da.Array,
                       subsample: int | float = 1,
                       return_indices: bool = False,
-                      random_state: np.random.RandomState | int | None = None):
+                      random_state: np.random.RandomState | int | None = None) -> np.ndarray:
     """
     Subsample a raster at valid values on out-of-memory chunks.
 
@@ -107,12 +127,9 @@ def delayed_subsample(darr: da.Array,
     rnd = _random_state_from_user_input(random_state=random_state)
 
     # Compute number of valid points for each block out-of-memory
-    def nb_valids_func(arr, axis, keepdims):
-        return np.count_nonzero(np.isfinite(arr), axis, keepdims=keepdims)
-
-    def keep_same(arr, axis, keepdims):
-        return arr
-    list_valids = da.reduction(darr, nb_valids_func, aggregate=keep_same, axis=None, dtype=np.int32).compute()
+    blocks = darr.to_delayed().ravel()
+    list_delayed_valids = [da.from_delayed(_delayed_nb_valids(b), shape=(1, 1), dtype=np.dtype("int32")) for b in blocks]
+    list_valids = np.concatenate([dask.compute(*list_delayed_valids)])
 
     # Sum to get total number of valid points
     total_valids = np.sum(list_valids)
@@ -121,37 +138,59 @@ def delayed_subsample(darr: da.Array,
     subsample_size = _get_subsample_size_from_user_input(subsample=subsample, total_valids=total_valids)
 
     # Get random 1D indexes for the subsample size
-    indices_1d = rnd.choice(subsample_size, total_valids, replace=False)
+    indices_1d = rnd.choice(total_valids, subsample_size, replace=False)
 
     # Sort which indexes belong to which chunk
-    ind_buckets = _get_indices_block_per_subsample(indices_1d, num_chunks=darr.numblocks, valids=list_valids)
+    ind_buckets = _get_indices_block_per_subsample(indices_1d, num_chunks=darr.numblocks, list_valids=list_valids)
 
-    # Create a delayed object for each block, and flatten the blocks into a 1d shape
-    blocks = darr.to_delayed().ravel()
+    # To just get the subsample without indices
+    if not return_indices:
+        # Task a delayed subsample to be computed for each block, skipping blocks with no values to sample
+        list_subsamples = [
+            _delayed_subsample_block(b, ind)
+            for i, (b, ind) in enumerate(zip(blocks, ind_buckets))
+            if len(ind_buckets[i]) > 0
+        ]
+        # Cast output to the right expected dtype and length, then compute and concatenate
+        list_subsamples_delayed = [da.from_delayed(s, shape=(list_valids[i]), dtype=darr.dtype)
+                                   for i, s in enumerate(list_subsamples)]
+        subsamples = np.concatenate(dask.compute(*list_subsamples_delayed), axis=0)
 
-    # Task a delayed subsample to be computed for each block, skipping blocks with no values to sample
-    list_subsamples = [
-        _delayed_subsample_block(b, ind)
-        for i, (b, ind) in enumerate(zip(blocks, ind_buckets))
-        if len(list_valids[i]) > 0
-    ]
+        return subsamples
 
-    # Cast output to the right expected dtype and length, then compute and concatenate
-    list_subsamples_delayed = [da.from_delayed(s, shape=(list_valids[i]), dtype=darr.dtype)
-                               for i, s in enumerate(list_subsamples)]
-    subsamples = np.concatenate(dask.compute(*list_subsamples_delayed), axis=0)
+    # To return indices
+    else:
+        # Get robust list of starts (using what is done in block_id of dask.array.map_blocks)
+        # https://github.com/dask/dask/blob/24493f58660cb933855ba7629848881a6e2458c1/dask/array/core.py#L908
+        from dask.utils import cached_cumsum
+        starts = [cached_cumsum(c, initial_zero=True) for c in darr.chunks]
+        num_chunks = darr.numblocks
+        # Get the starts per 1D block ID by unravelling starting indexes for each block
+        indexes_xi, indexes_yi = np.unravel_index(np.arange(len(blocks)), shape=(num_chunks[0], num_chunks[1]))
+        block_ids = [{"xstart": starts[0][indexes_xi[i]], "ystart": starts[1][indexes_yi[i]]} for i in range(len(blocks))]
 
-    return subsamples
+        # Task delayed subsample indices to be computed for each block, skipping blocks with no values to sample
+        list_subsample_indices = [
+            _delayed_subsample_indices_block(b, ind, block_id=block_ids[i])
+            for i, (b, ind) in enumerate(zip(blocks, ind_buckets))
+            if len(ind_buckets[i]) > 0
+        ]
+        # Cast output to the right expected dtype and length, then compute and concatenate
+        list_subsamples_indices_delayed = [da.from_delayed(s, shape=(2, len(ind_buckets[i])), dtype=np.dtype("int32"))
+                                           for i, s in enumerate(list_subsample_indices)]
+        indices = np.concatenate(dask.compute(*list_subsamples_indices_delayed), axis=0)
+
+        return indices[:, 0], indices[:, 1]
 
 
 # 2/ POINT INTERPOLATION ON REGULAR OR EQUAL GRID
 # This functionality is not covered efficiently by Dask/Xarray, because they need to support rectilinear grids, which
-# is difficult when interpolating in the chunked dimensions, loading nearly all array memory when using .interp().
+# is difficult when interpolating in the chunked dimensions, and loads nearly all array memory when using .interp().
 
 # Here we harness the fact that rasters are always on regular (or sometimes equal) grids to efficiently map
 # the location of the blocks required for interpolation, which requires little memory usage.
 
-# Code inspired by https://blog.dask.org/2021/07/02/ragged-output and the "block_id" in map_blocks
+# Code structure inspired by https://blog.dask.org/2021/07/02/ragged-output and the "block_id" in map_blocks
 
 def _get_interp_indices_per_block(interp_x, interp_y, starts, num_chunks, chunksize, xres, yres):
     """Map blocks where each pair of interpolation coordinates will have to be computed."""
@@ -173,7 +212,7 @@ def _get_interp_indices_per_block(interp_x, interp_y, starts, num_chunks, chunks
 
 
 @dask.delayed
-def _delayed_interp_block(arr_chunk: np.ndarray, block_id: dict[str, Any], interp_coords: tuple[np.ndarray, np.ndarray]) -> np.ndarray:
+def _delayed_interp_block(arr_chunk: np.ndarray, block_id: dict[str, Any], interp_coords: np.ndarray) -> np.ndarray:
     """
     Interpolate block in 2D out-of-memory for a regular or equal grid.
     """
@@ -188,7 +227,7 @@ def _delayed_interp_block(arr_chunk: np.ndarray, block_id: dict[str, Any], inter
     # TODO: Use scipy.map_coordinates for an equal grid as in Raster.interp_points?
 
     # Interpolate to points
-    interp_chunk = interpn(points=(x_coords, y_coords), values=arr_chunk, xi=interp_coords)
+    interp_chunk = interpn(points=(x_coords, y_coords), values=arr_chunk, xi=(interp_coords[0, :], interp_coords[1, :]))
 
     # And return the interpolated array
     return interp_chunk
@@ -196,7 +235,7 @@ def _delayed_interp_block(arr_chunk: np.ndarray, block_id: dict[str, Any], inter
 def delayed_interp_points(darr: da.Array,
                           points: tuple[list[float], list[float]],
                           resolution = tuple[float, float],
-                          method: Literal["nearest", "linear", "cubic", "quintic"] = "linear"):
+                          method: Literal["nearest", "linear", "cubic", "quintic"] = "linear") -> np.ndarray:
     """
     Interpolate raster at point coordinates on out-of-memory chunks.
 
@@ -213,6 +252,9 @@ def delayed_interp_points(darr: da.Array,
     :return: Array of raster value(s) for the given points.
     """
 
+    # Convert input to 2D array
+    points = np.vstack((points[0], points[1]))
+
     # Map depth of overlap required for each interpolation method
     # TODO: Double-check this window somewhere in SciPy's documentation
     map_depth = {"nearest": 1, "linear": 2, "cubic": 3, "quintic": 5}
@@ -228,7 +270,7 @@ def delayed_interp_points(darr: da.Array,
     num_chunks = expanded.numblocks
 
     # Get samples indices per blocks
-    indices = _get_interp_indices_per_block(points[0], points[1], starts, num_chunks, chunksize, resolution[0], resolution[1])
+    indices = _get_interp_indices_per_block(points[0, :], points[1, :], starts, num_chunks, chunksize, resolution[0], resolution[1])
 
     # Create a delayed object for each block, and flatten the blocks into a 1d shape
     blocks = expanded.to_delayed().ravel()
@@ -244,20 +286,30 @@ def delayed_interp_points(darr: da.Array,
     # Compute values delayed
     list_interp = [_delayed_interp_block(blocks[i],
                                     block_ids[i],
-                                    points[indices[i], :])
+                                    points[:, indices[i]])
                                     for i, data_chunk in enumerate(blocks)
                                     if len(indices[i]) > 0
               ]
 
     # Use np.nan for unknown chunk sizes https://dask.pydata.org/en/latest/array-chunks.html#unknown-chunks
     list_interp_delayed = [da.from_delayed(p, shape=(1, len(indices[i])), dtype=darr.dtype) for i, p in enumerate(list_interp)]
-    points = np.concatenate(dask.compute(*list_interp_delayed), axis=0)
+    interp_points = np.concatenate(dask.compute(*list_interp_delayed), axis=0)
 
     # Re-order per-block output points to match their original indices
     indices = np.concatenate(indices).astype(int)
     argsort = np.argsort(indices)
-    points = np.array(points)[argsort]
+    interp_points = np.array(interp_points)[argsort]
 
+    return interp_points
 
 # 3/ TERRAIN ATTRIBUTES AND APPLY_MATRIX
 # Output array is same shape as input, so we can use directly map_overlap here!
+
+# def delayed_get_terrain_attribute(darr: da.Array, attribute: str | list[str], resolution: float | tuple[float], **kwargs):
+#
+#     def partial_get_terrain_attribute(arr: np.ndarray):
+#         return get_terrain_attribute(arr, attribute=attribute, resolution=resolution, **kwargs)
+#
+#     map_depth_attribute: {}
+#
+#     dask.array.map_overlap(partial_get_terrain_attribute, depth=)
