dfb62fed3162344df94529a36fadf555a7649d96
Romain Hugonnet <romain.hugonnet@gmail.com>
2024-04-22 16:13:12 -0800

    Update docstrings and comments to be clearer

--- xdem/coreg/delayed.py
+++ xdem/coreg/delayed.py
@@ -14,11 +14,12 @@ import dask.array as da
 from scipy.interpolate import interpn
 
 # 1/ SUBSAMPLING
-# Getting an exact subsample size only for valid values with small memory usage using dask.delayed
-# This is not trivial because of ragged output considerations (varying output length)
-# see https://blog.dask.org/2021/07/02/ragged-output
-# The function dask.array.map_blocks creates larger RAM usage by having to drop an axis (re-chunk along 1D of the
-# 2D array), so we use our own dask.delayed implementation
+# Getting an exact subsample size out-of-memory only for valid values is not supported directly by Dask/Xarray
+
+# It is not trivial because we don't know where valid values will be in advance, and because of ragged output (varying 
+# output length considerations), which prevents from using high-level functions with good efficiency
+# We thus follow https://blog.dask.org/2021/07/02/ragged-output (the dask.array.map_blocks solution has a larger RAM 
+# usage by having to drop an axis and re-chunk along 1D of the 2D array, so we use the dask.delayed solution instead)
 
 def _random_state_from_user_input(random_state: np.random.RandomState | int | None = None) -> np.random.RandomState:
     """Define random state based on varied user input."""
@@ -34,33 +35,46 @@ def _random_state_from_user_input(random_state: np.random.RandomState | int | No
 
     return rnd
 
-def _get_subsample_size_from_user_input(subsample: int | float, total_valids: int) -> int:
+def _get_subsample_size_from_user_input(subsample: int | float, total_nb_valids: int) -> int:
     """Get subsample size based on a user input of either integer size or fraction of the number of valid points."""
 
     if (subsample <= 1) & (subsample > 0):
-        npoints = int(subsample * total_valids)
+        npoints = int(subsample * total_nb_valids)
     elif subsample > 1:
         # Use the number of valid points if larger than subsample asked by user
-        npoints = min(int(subsample), total_valids)
-        if subsample > total_valids:
-            warnings.warn(f"Subsample value of {subsample} is larger than the number of valid pixels of {total_valids},"
+        npoints = min(int(subsample), total_nb_valids)
+        if subsample > total_nb_valids:
+            warnings.warn(f"Subsample value of {subsample} is larger than the number of valid pixels of {total_nb_valids},"
                           f"using all valid pixels as a subsample.", category=UserWarning)
     else:
         raise ValueError("Subsample must be > 0.")
 
     return npoints
 
-def _get_indices_block_per_subsample(xxs: np.ndarray, num_chunks: tuple[int, int], list_valids: list[int]) -> list[list[int]]:
-    """Get list of block-relative 1D subsample indexes for each block."""
+def _get_indices_block_per_subsample(xxs: np.ndarray, num_chunks: tuple[int, int], nb_valids_per_block: list[int]) -> list[list[int]]:
+    """
+    Get list of 1D valid subsample indices relative to the block for each block.
+
+    The 1D valid subsample indices correspond to the subsample index to apply for a flattened array of valid values.
+    Relative to the block means converted so that the block indexes for valid values starts at 0 up to the number of
+    valid values in that block (while the input indices go from zero to the total number of valid values in the full
+    array).
+
+    :param xxs: Subsample 1D indexes among a total number of valid values.
+    :param num_chunks: Number of chunks in X and Y.
+    :param nb_valids_per_block: Number of valid pixels per block.
+
+    :returns: Relative 1D valid subsample index per block.
+    """
 
     # Apply a cumulative sum to get the first 1D total index of each block
-    valids_cumsum = np.cumsum(list_valids)
+    valids_cumsum = np.cumsum(nb_valids_per_block)
 
     # We can write a faster algorithm by sorting
     xxs = np.sort(xxs)
 
-    # We define a list of indices bucket per block.
-    ind_buckets = [[] for _ in range((num_chunks[0] * num_chunks[1]))]
+    # We define a list of indices per block
+    relative_ind_per_block = [[] for _ in range((num_chunks[0] * num_chunks[1]))]
     k = 0  # K is the block number
     for x in xxs:
 
@@ -71,9 +85,9 @@ def _get_indices_block_per_subsample(xxs: np.ndarray, num_chunks: tuple[int, int
         # Add 1D subsample index  relative to first subsample index of this block
         first_xindex_block = valids_cumsum[k - 1] if k >= 1 else 0  # The first 1D valid subsample index of the block
         relative_xindex = x - first_xindex_block
-        ind_buckets[k].append(relative_xindex)
+        relative_ind_per_block[k].append(relative_xindex)
 
-    return ind_buckets
+    return relative_ind_per_block
 
 @dask.delayed
 def _delayed_nb_valids(arr_chunk: np.ndarray) -> np.ndarray:
@@ -82,7 +96,7 @@ def _delayed_nb_valids(arr_chunk: np.ndarray) -> np.ndarray:
 
 @dask.delayed
 def _delayed_subsample_block(arr_chunk: np.ndarray, subsample_indices: np.ndarray) -> np.ndarray:
-    """Subsample the values at the corresponding indices per block."""
+    """Subsample the valid values at the corresponding 1D valid indices per block."""
 
     s_chunk = arr_chunk[np.isfinite(arr_chunk)][subsample_indices]
 
@@ -90,7 +104,7 @@ def _delayed_subsample_block(arr_chunk: np.ndarray, subsample_indices: np.ndarra
 
 @dask.delayed
 def _delayed_subsample_indices_block(arr_chunk: np.ndarray, subsample_indices: np.ndarray, block_id: dict[str, Any]) -> np.ndarray:
-    """Return 2D indices at the subsampled 1D indices per block."""
+    """Return 2D indices from the subsampled 1D valid indices per block."""
 
     # Unravel indices of valid data to the shape of the block
     ix, iy = np.unravel_index(np.argwhere(np.isfinite(arr_chunk.flatten()))[subsample_indices], shape=arr_chunk.shape)
@@ -108,11 +122,20 @@ def delayed_subsample(darr: da.Array,
     """
     Subsample a raster at valid values on out-of-memory chunks.
 
-    First, the number of valid values in each chunk are identified.
-    Then, the number of values to be sampled is partitioned randomly among the chunks, giving them a flattened index
-    of where to sample among their valid values.
+    Optionally, this function can return the 2D indices of the subsample of valid values instead.
 
-    Optionally, this function can return the index of the subsample.
+    The random subsample is distributed evenly across valid values, no matter which chunk they belong to.
+    First, the number of valid values in each chunk are computed out-of-memory. Then, a subsample is defined among
+    the total number of valid values, which are then indexed sequentially along the chunk valid values out-of-memory.
+
+    A random state will give a fixed subsample for a delayed array with a fixed chunksize. However, the subsample
+    will vary with changing chunksize because the 1D delayed indexing depends on it (indexing per valid value per
+    flattened chunk). For this reason, a loaded array will also have a different subsample due to its direct 1D
+    indexing (per valid value for the entire flattened array).
+
+    To ensure you re-use a similar subsample of valid values for several arrays, call this function with
+    return_indices=True, then sample your arrays out-of-memory with .vindex[indices[0], indices[1]]
+    (this assumes that these arrays have valid values at the same locations).
 
     :param darr: Input dask array.
     :param subsample: Subsample size. If <= 1, will be considered a fraction of valid pixels to extract.
@@ -129,30 +152,30 @@ def delayed_subsample(darr: da.Array,
     # Compute number of valid points for each block out-of-memory
     blocks = darr.to_delayed().ravel()
     list_delayed_valids = [da.from_delayed(_delayed_nb_valids(b), shape=(1, 1), dtype=np.dtype("int32")) for b in blocks]
-    list_valids = np.concatenate([dask.compute(*list_delayed_valids)])
+    nb_valids_per_block = np.concatenate([dask.compute(*list_delayed_valids)])
 
     # Sum to get total number of valid points
-    total_valids = np.sum(list_valids)
+    total_nb_valids = np.sum(nb_valids_per_block)
 
     # Get subsample size (depending on user input)
-    subsample_size = _get_subsample_size_from_user_input(subsample=subsample, total_valids=total_valids)
+    subsample_size = _get_subsample_size_from_user_input(subsample=subsample, total_nb_valids=total_nb_valids)
 
     # Get random 1D indexes for the subsample size
-    indices_1d = rnd.choice(total_valids, subsample_size, replace=False)
+    indices_1d = rnd.choice(total_nb_valids, subsample_size, replace=False)
 
     # Sort which indexes belong to which chunk
-    ind_buckets = _get_indices_block_per_subsample(indices_1d, num_chunks=darr.numblocks, list_valids=list_valids)
+    ind_per_block = _get_indices_block_per_subsample(indices_1d, num_chunks=darr.numblocks, nb_valids_per_block=nb_valids_per_block)
 
     # To just get the subsample without indices
     if not return_indices:
         # Task a delayed subsample to be computed for each block, skipping blocks with no values to sample
         list_subsamples = [
             _delayed_subsample_block(b, ind)
-            for i, (b, ind) in enumerate(zip(blocks, ind_buckets))
-            if len(ind_buckets[i]) > 0
+            for i, (b, ind) in enumerate(zip(blocks, ind_per_block))
+            if len(ind_per_block[i]) > 0
         ]
         # Cast output to the right expected dtype and length, then compute and concatenate
-        list_subsamples_delayed = [da.from_delayed(s, shape=(list_valids[i]), dtype=darr.dtype)
+        list_subsamples_delayed = [da.from_delayed(s, shape=(nb_valids_per_block[i]), dtype=darr.dtype)
                                    for i, s in enumerate(list_subsamples)]
         subsamples = np.concatenate(dask.compute(*list_subsamples_delayed), axis=0)
 
@@ -172,11 +195,11 @@ def delayed_subsample(darr: da.Array,
         # Task delayed subsample indices to be computed for each block, skipping blocks with no values to sample
         list_subsample_indices = [
             _delayed_subsample_indices_block(b, ind, block_id=block_ids[i])
-            for i, (b, ind) in enumerate(zip(blocks, ind_buckets))
-            if len(ind_buckets[i]) > 0
+            for i, (b, ind) in enumerate(zip(blocks, ind_per_block))
+            if len(ind_per_block[i]) > 0
         ]
         # Cast output to the right expected dtype and length, then compute and concatenate
-        list_subsamples_indices_delayed = [da.from_delayed(s, shape=(2, len(ind_buckets[i])), dtype=np.dtype("int32"))
+        list_subsamples_indices_delayed = [da.from_delayed(s, shape=(2, len(ind_per_block[i])), dtype=np.dtype("int32"))
                                            for i, s in enumerate(list_subsample_indices)]
         indices = np.concatenate(dask.compute(*list_subsamples_indices_delayed), axis=0)
 
@@ -202,13 +225,13 @@ def _get_interp_indices_per_block(interp_x, interp_y, starts, num_chunks, chunks
     #  (as it uses array instead of nested lists, and nested lists grow in RAM very fast)
 
     # We use one bucket per block, assuming a flattened blocks shape
-    ind_buckets = [[] for _ in range((num_chunks[0] * num_chunks[1]))]
+    ind_per_block = [[] for _ in range((num_chunks[0] * num_chunks[1]))]
     for i, (x, y) in enumerate(zip(interp_x, interp_y)):
         # Because it is a regular grid, we know exactly in which block ID the coordinate will fall
         block_i_1d = int((x - starts[0][0]) / (xres * chunksize[0])) * num_chunks[1] + int((y - starts[1][0])/ (yres * chunksize[1]))
-        ind_buckets[block_i_1d].append(i)
+        ind_per_block[block_i_1d].append(i)
 
-    return ind_buckets
+    return ind_per_block
 
 
 @dask.delayed
@@ -234,7 +257,7 @@ def _delayed_interp_block(arr_chunk: np.ndarray, block_id: dict[str, Any], inter
 
 def delayed_interp_points(darr: da.Array,
                           points: tuple[list[float], list[float]],
-                          resolution = tuple[float, float],
+                          resolution: tuple[float, float],
                           method: Literal["nearest", "linear", "cubic", "quintic"] = "linear") -> np.ndarray:
     """
     Interpolate raster at point coordinates on out-of-memory chunks.
@@ -270,7 +293,7 @@ def delayed_interp_points(darr: da.Array,
     num_chunks = expanded.numblocks
 
     # Get samples indices per blocks
-    indices = _get_interp_indices_per_block(points[0, :], points[1, :], starts, num_chunks, chunksize, resolution[0], resolution[1])
+    ind_per_block = _get_interp_indices_per_block(points[0, :], points[1, :], starts, num_chunks, chunksize, resolution[0], resolution[1])
 
     # Create a delayed object for each block, and flatten the blocks into a 1d shape
     blocks = expanded.to_delayed().ravel()
@@ -286,17 +309,17 @@ def delayed_interp_points(darr: da.Array,
     # Compute values delayed
     list_interp = [_delayed_interp_block(blocks[i],
                                     block_ids[i],
-                                    points[:, indices[i]])
+                                    points[:, ind_per_block[i]])
                                     for i, data_chunk in enumerate(blocks)
-                                    if len(indices[i]) > 0
+                                    if len(ind_per_block[i]) > 0
               ]
 
     # Use np.nan for unknown chunk sizes https://dask.pydata.org/en/latest/array-chunks.html#unknown-chunks
-    list_interp_delayed = [da.from_delayed(p, shape=(1, len(indices[i])), dtype=darr.dtype) for i, p in enumerate(list_interp)]
+    list_interp_delayed = [da.from_delayed(p, shape=(1, len(ind_per_block[i])), dtype=darr.dtype) for i, p in enumerate(list_interp)]
     interp_points = np.concatenate(dask.compute(*list_interp_delayed), axis=0)
 
     # Re-order per-block output points to match their original indices
-    indices = np.concatenate(indices).astype(int)
+    indices = np.concatenate(ind_per_block).astype(int)
     argsort = np.argsort(indices)
     interp_points = np.array(interp_points)[argsort]
 
