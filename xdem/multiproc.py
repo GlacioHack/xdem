090bc62ff02a6abb0f6a9ae200320663d9a0b452
vschaffn <valentin.schaffner@cs-soprasteria.com>
2025-03-21 18:11:29 +0100

    complete docstrings

--- xdem/multiproc.py
+++ xdem/multiproc.py
@@ -1,23 +1,56 @@
+# Copyright (c) 2024 xDEM developers
+# Copyright (c) 2025 Centre National d'Etudes Spatiales (CNES)
+#
+# This file is part of the xDEM project:
+# https://github.com/glaciohack/xdem
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+#
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Terrain attribute calculations using multiprocessing."""
+from pathlib import Path
+
 import numpy as np
 import rasterio as rio
 from geoutils._typing import NDArrayNum
-from geoutils.raster import RasterType, compute_tiling
-from geoutils.raster.distributed_computing.cluster import (
-    AbstractCluster,
-    ClusterGenerator,
-)
+from geoutils.raster import Raster, RasterType, compute_tiling
+from geoutils.raster.distributed_computing import AbstractCluster, ClusterGenerator
 
-from xdem import DEM
 from xdem.terrain import get_terrain_attribute
 
 
-def get_raster_tile(raster_unload: RasterType, tile: NDArrayNum) -> RasterType:
+def load_raster_tile(raster_unload: RasterType, tile: NDArrayNum) -> RasterType:
+    """
+    Extracts a specific tile (spatial subset) from the raster based on the provided tile coordinates.
+
+    :param raster_unload: The input raster from which the tile is to be extracted.
+    :param tile: The bounding box of the tile as [xmin, xmax, ymin, ymax].
+    :return: The extracted raster tile.
+    """
     xmin, xmax, ymin, ymax = tile
     raster_tile = raster_unload.icrop(bbox=(xmin, ymin, xmax, ymax))
     return raster_tile
 
 
-def remove_tile_padding(dem: DEM, raster_tile: RasterType, tile: NDArrayNum, padding: int) -> None:
+def remove_tile_padding(dem: RasterType, raster_tile: RasterType, tile: NDArrayNum, padding: int) -> None:
+    """
+    Removes the padding added around tiles during terrain attribute computation to prevent edge effects.
+
+    :param dem: The full DEM object from which tiles are extracted.
+    :param raster_tile: The raster tile with possible padding that needs removal.
+    :param tile: The bounding box of the tile as [xmin, xmax, ymin, ymax].
+    :param padding: The padding size to be removed from each side of the tile.
+    """
     # Remove padding if the tile is not on the edge of the DEM
     xmin, xmax, ymin, ymax = 0, raster_tile.height, 0, raster_tile.width
     if tile[0] != 0:
@@ -36,9 +69,10 @@ def remove_tile_padding(dem: DEM, raster_tile: RasterType, tile: NDArrayNum, pad
 
 
 def get_terrain_attribute_multiproc(
-    dem_path: str,
+    dem: str | RasterType,
     attribute: str | list[str],
     tile_size: int,
+    out_dir: str | None = None,
     resolution: tuple[float, float] | float | None = None,
     degrees: bool = True,
     hillshade_altitude: float = 45.0,
@@ -52,11 +86,14 @@ def get_terrain_attribute_multiproc(
     cluster: AbstractCluster | None = None,
 ) -> None:
     """
-    See description of terrain.get_terrain_attribute.
+    Compute terrain attributes (e.g., slope, aspect, hillshade) in parallel using tiling and multiprocessing.
+    Saves the terrain attribute raster(s) as a GeoTIFF in out_dir or next to the original DEM by default.
+    See description of :func:`xdem.DEM.get_terrain_attribute` for more information.
 
-    :param dem_path: The DEM to analyze.
+    :param dem: The DEM to analyze.
     :param attribute: The terrain attribute(s) to calculate.
     :param tile_size: Size of each tile in pixels (square tiles).
+    :param out_dir: Saving directory for the terrain attribute rasters(s).
     :param resolution: The X/Y or (X, Y) resolution of the DEM.
     :param degrees: Convert radians to degrees?
     :param hillshade_altitude: The shading altitude in degrees (0-90°). 90° is straight from above.
@@ -72,15 +109,14 @@ def get_terrain_attribute_multiproc(
             execute without parallelism.
 
     :raises ValueError: If the inputs are poorly formatted or are invalid.
-
-    :returns: One or multiple arrays of the requested attribute(s)
     """
     if cluster is None:
         cluster = ClusterGenerator("basic")  # type: ignore
     assert cluster is not None  # for mypy
 
-    # load dem metadata
-    dem = DEM(dem_path)
+    # load dem metadata if not already loaded
+    if isinstance(dem, str):
+        dem = Raster(dem)
 
     # Generate tiling grid
     # Adapt the overlap if needed
@@ -91,8 +127,13 @@ def get_terrain_attribute_multiproc(
     if isinstance(attribute, str):
         attribute = [attribute]
     for attr in attribute:
-        # The attr will be saved next to the DEM under the name "dem_attr"
-        outfile = dem_path.removesuffix(".tif") + "_" + attr + ".tif"
+        dem_path = Path(dem.filename)
+        filename = dem_path.stem + "_" + attr + ".tif"
+        # The attr will be saved in out_dir or next to the DEM under the name "dem_path_attr.tif"
+        if out_dir is None:
+            outfile = dem_path.parent.joinpath(filename)
+        else:
+            outfile = Path(out_dir).joinpath(filename)
 
         # Create tasks for multiprocessing
         tasks = []
@@ -105,7 +146,7 @@ def get_terrain_attribute_multiproc(
                         fun=terrain_attribute_by_block,
                         args=[
                             dem,
-                            attribute,
+                            attr,
                             tile,
                             overlap,
                             resolution,
@@ -122,6 +163,9 @@ def get_terrain_attribute_multiproc(
                     )
                 )
 
+        # get first tile to retrieve dtype and nodata
+        attr_tile1, _ = cluster.get_res(tasks[1])
+
         with rio.open(
             outfile,
             "w",
@@ -129,10 +173,10 @@ def get_terrain_attribute_multiproc(
             height=dem.height,
             width=dem.width,
             count=dem.count,
-            dtype=dem.dtype,
+            dtype=attr_tile1.dtype,
             crs=dem.crs,
             transform=dem.transform,
-            nodata=dem.nodata,
+            nodata=attr_tile1.nodata,
         ) as dst:
             try:
                 # Retrieve terrain attribute computation results
@@ -174,8 +218,28 @@ def terrain_attribute_by_block(
     edge_method: str = "none",
     window_size: int = 3,
 ) -> tuple[RasterType, NDArrayNum]:
+    """
+    Calculate a terrain attribute for a specific tile (spatial subset) of the DEM.
+
+    :param dem: The input DEM as a raster object.
+    :param attribute: The terrain attribute to compute (e.g., slope, aspect, hillshade).
+    :param tile: The bounding box of the tile as [xmin, xmax, ymin, ymax].
+    :param overlap: The overlap (in pixels) to add to tiles to prevent edge artifacts.
+    :param resolution: The spatial resolution of the DEM or None for the default resolution.
+    :param degrees: Whether to convert angle-based attributes to degrees (True) or keep in radians.
+    :param hillshade_altitude: The sun altitude angle for hillshade (used if hillshade is the attribute).
+    :param hillshade_azimuth: The sun azimuth angle for hillshade.
+    :param hillshade_z_factor: Vertical exaggeration factor for hillshade calculations.
+    :param slope_method: Method for slope calculation. Options: "Horn", "ZevenbergThorne".
+    :param tri_method: Method for Terrain Ruggedness Index (TRI) calculation: "Riley" or "Wilson".
+    :param fill_method: Edge/corner filling method for missing data.
+    :param edge_method: Handling method for edges during computation.
+    :param window_size: The size of the moving window for windowed terrain attributes.
+
+    :return: A tuple containing the raster object with the computed attribute and the tile coordinates.
+    """
     # Load tile
-    dem_tile = get_raster_tile(dem, tile)
+    dem_tile = load_raster_tile(dem, tile)
 
     # Compute terrain attribute
     dem_tile_attr = get_terrain_attribute(
