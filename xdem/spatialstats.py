04ebe09dd3731e9d8633acec71f6f93d899d3c58
vschaffn <valentin.schaffner@cs-soprasteria.com>
2025-04-10 11:00:56 +0200

    deprecate nmad

--- xdem/spatialstats.py
+++ xdem/spatialstats.py
@@ -28,6 +28,7 @@ import warnings
 from typing import Any, Callable, Iterable, Literal, TypedDict, overload
 
 import geopandas as gpd
+import geoutils as gu
 import matplotlib
 import matplotlib.colors as colors
 import matplotlib.pyplot as plt
@@ -36,9 +37,9 @@ import numpy as np
 import pandas as pd
 from geoutils.raster import Mask, Raster, RasterType, subsample_array
 from geoutils.raster.array import get_array_and_mask
-from geoutils.stats import nmad
 from geoutils.vector.vector import Vector, VectorType
 from numpy.typing import ArrayLike
+from packaging.version import Version
 from scipy import integrate
 from scipy.interpolate import RegularGridInterpolator, griddata
 from scipy.optimize import curve_fit
@@ -48,10 +49,29 @@ from scipy.stats import binned_statistic, binned_statistic_2d, binned_statistic_
 from skimage.draw import disk
 
 from xdem._typing import NDArrayf
+from xdem.misc import deprecate
 
 with warnings.catch_warnings():
     warnings.filterwarnings("ignore", category=DeprecationWarning)
     import skgstat as skg
+
+
+@deprecate(
+    removal_version=Version("0.4"), details="xdem.spatialstats.nmad is being deprecated in favor of geoutils.stats.nmad"
+)
+def nmad(data: NDArrayf, nfact: float = 1.4826) -> np.floating[Any]:
+    """
+    Calculate the normalized median absolute deviation (NMAD) of an array.
+    Default scaling factor is 1.4826 to scale the median absolute deviation (MAD) to the dispersion of a normal
+    distribution (see https://en.wikipedia.org/wiki/Median_absolute_deviation#Relation_to_standard_deviation, and
+    e.g. Höhle and Höhle (2009), http://dx.doi.org/10.1016/j.isprsjprs.2009.02.003)
+
+    :param data: Input array or raster
+    :param nfact: Normalization factor for the data
+
+    :returns nmad: (normalized) median absolute deviation of data.
+    """
+    return gu.stats.nmad(data, nfact)
 
 
 def nd_binning(
