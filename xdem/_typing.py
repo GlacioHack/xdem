7394c4fa715b6a6d42feb414e8a9ea5f7e839b9a
Romain Hugonnet <romain.hugonnet@gmail.com>
2025-04-13 12:47:58 +0200

    Make terrain attribute output dtype explicit

--- xdem/_typing.py
+++ xdem/_typing.py
@@ -26,13 +26,22 @@ import numpy as np
 # Only for Python >= 3.9
 if sys.version_info.minor >= 9:
 
-    from numpy.typing import NDArray  # this syntax works starting on Python 3.9
+    from numpy.typing import NDArray, DTypeLike, ArrayLike  # this syntax works starting on Python 3.9
+
+    # Simply define here if they exist
+    DTypeLike = DTypeLike
+    ArrayLike = ArrayLike
 
     NDArrayf = NDArray[np.floating[Any]]
     NDArrayb = NDArray[np.bool_]
     MArrayf = np.ma.masked_array[Any, np.dtype[np.floating[Any]]]
 
 else:
+
+    # Make an array-like type (since the array-like numpy type only exists in numpy>=1.20)
+    DTypeLike = Union[str, type, np.dtype]  # type: ignore
+    ArrayLike = Union[np.ndarray, np.ma.masked_array, List[Any], Tuple[Any]]  # type: ignore
+
     NDArrayf = np.ndarray  # type: ignore
     NDArrayb = np.ndarray  # type: ignore
     MArrayf = np.ma.masked_array  # type: ignore
