ff2a0080112a454b87f0b874001dc12fce1c4054
Romain Hugonnet <romain.hugonnet@gmail.com>
2026-01-06 16:12:24 -0900

    Try like this

--- xdem/cli.py
+++ xdem/cli.py
@@ -26,13 +26,17 @@ from xdem._misc import import_optional
 from xdem.workflows import Accuracy, Topo
 from xdem.workflows.schemas import COMPLETE_CONFIG_ACCURACY, COMPLETE_CONFIG_TOPO
 
-lib_gobject_name = ctypes.util.find_library("gobject-2.0")
-lib_pango_name = ctypes.util.find_library("pango-1.0")
-if lib_gobject_name and lib_pango_name:
-    from weasyprint import HTML
-    _has_libgobject = True
-else:
-    _has_libgobject = False
+try:
+    lib_gobject_name = ctypes.util.find_library("gobject-2.0")
+    lib_pango_name = ctypes.util.find_library("pango-1.0")
+    if lib_gobject_name and lib_pango_name:
+        from weasyprint import HTML
+        _has_libgobject = True
+    else:
+        _has_libgobject = False
+    _has_weasyprint = _has_libgobject
+except ImportError:
+    _has_weasyprint = False
 
 def main() -> None:
     """
@@ -111,7 +115,7 @@ def main() -> None:
         raise ValueError(f"{args.command} doesn't exist, valid command are 'accuracy', 'topo'")
 
     if args.config:
-        if not _has_libgobject:
+        if not _has_weasyprint:
             msg = ("Optional dependency 'weasyprint' required. "
                    "Install it directly or through: pip install xdem[opt].")
             raise ImportError(msg)
